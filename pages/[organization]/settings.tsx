b42f3da037061a312af272f7b215a4b5392cba1c
Aliez <renzholy@gmail.com>
2023-01-01 13:56:31 +0800

    init form

--- pages/[organization]/settings.tsx
+++ pages/[organization]/settings.tsx
@@ -1,6 +1,9 @@
 import { createInstance } from 'dotbit'
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
 import useSWR from 'swr'
+import AvatarInput from '../../components/avatar-input'
 import useArweaveFile from '../../hooks/use-arweave-file'
 import { Organization } from '../../src/schemas'
 
@@ -17,10 +20,35 @@ export default function OrganizationSettingsPage() {
     },
   )
   const { data } = useArweaveFile<Organization>(hash)
+  const { register, watch, handleSubmit, reset, setValue } =
+    useForm<Organization>()
+  useEffect(() => {
+    reset(data)
+  }, [data, reset])
+  const onSubmit = console.log
 
-  return (
-    <pre>
-      <code>{JSON.stringify(data, null, 2)}</code>
-    </pre>
-  )
+  return organization ? (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>avatar</label>
+      <AvatarInput
+        did={organization}
+        value={watch('profile.avatar')}
+        onChange={(avatar) => setValue('profile.avatar', avatar)}
+      />
+      <br />
+      <label>name</label>
+      <input {...register('profile.name')} />
+      <br />
+      <label>about</label>
+      <input {...register('profile.about')} />
+      <br />
+      <label>website</label>
+      <input {...register('profile.website')} />
+      <br />
+      <label>term of service</label>
+      <input {...register('profile.tos')} />
+      <br />
+      <input type="submit" />
+    </form>
+  ) : null
 }
