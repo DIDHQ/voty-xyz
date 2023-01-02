e57515e0c391dbc4cecf8189ba3bd7a5edca5777
Aliez <renzholy@gmail.com>
2023-01-02 17:32:40 +0800

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,31 @@
-export const chain_id_to_rpc: { [chain_id: number]: string } = {
-  1: 'https://rpc.ankr.com/eth',
-  56: 'https://rpc.ankr.com/bsc',
-  137: 'https://rpc.ankr.com/polygon',
+export const common_coin_types = {
+  ETH: 60,
+  TRX: 195,
+  CKB: 309,
+  MATIC: 966,
+  BSC: 9006,
 }
 
-export const chain_id_to_coin_type: { [coin_type: number]: number } = {
-  1: 60,
-  56: 9006,
-  137: 966,
+export const common_chain_ids = {
+  ETH: 1,
+  BSC: 56,
+  MATIC: 137,
+}
+
+export const chain_id_to_rpc: { [chain_id: number]: string } = {
+  [common_chain_ids.ETH]: 'https://rpc.ankr.com/eth',
+  [common_chain_ids.BSC]: 'https://rpc.ankr.com/bsc',
+  [common_chain_ids.MATIC]: 'https://rpc.ankr.com/polygon',
+}
+
+export const chain_id_to_coin_type: { [chain_id: number]: number } = {
+  [common_chain_ids.ETH]: common_coin_types.ETH,
+  [common_chain_ids.BSC]: common_coin_types.BSC,
+  [common_chain_ids.MATIC]: common_coin_types.MATIC,
 }
 
 export const coin_type_to_chain_id: { [coin_type: number]: number } = {
-  60: 1,
-  966: 137,
-  9006: 56,
+  [common_coin_types.ETH]: common_chain_ids.ETH,
+  [common_coin_types.MATIC]: common_chain_ids.MATIC,
+  [common_coin_types.BSC]: common_chain_ids.BSC,
 }
