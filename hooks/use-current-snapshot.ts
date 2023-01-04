ae1ed46d188c5f3a70a3ff82e901c2f257d10dee
Aliez <renzholy@gmail.com>
2023-01-04 18:06:05 +0800

    improve signature (#9)

--- hooks/use-current-snapshot.ts
+++ hooks/use-current-snapshot.ts
@@ -1,7 +1,7 @@
 import useSWR from 'swr'
 import { getCurrentSnapshot } from '../src/snapshot'
 
-export function useCurrentSnapshot(coinType?: number) {
+export default function useCurrentSnapshot(coinType?: number) {
   return useSWR(
     coinType === undefined ? null : ['current snapshot', coinType],
     () => {
