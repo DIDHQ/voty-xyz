7918d6e26f55fd5700bccc106a0cf3a4f4a9a86c
mozwell <59408225+mozwell@users.noreply.github.com>
2023-01-04 12:15:33 +0800

    feat: add UI libs (#5)
    * feat:add tailwind and daisyUI
    
    * feat:add clsx & icon-park & button component
    
    * fix:update yarn.lock to use yarnpkg as source
    
    * fix:add react-daisyui & fix PR issues

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['dark', 'light'],
+  },
+}
