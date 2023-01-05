959acd3d9a22a68f950146dfd6b24d328b934796
Aliez <renzholy@gmail.com>
2023-01-05 18:51:31 +0800

    Update create.tsx

--- pages/[organization]/workgroup/[workgroup]/create.tsx
+++ pages/[organization]/workgroup/[workgroup]/create.tsx
@@ -1,10 +1,16 @@
 import { Input, Textarea } from 'react-daisyui'
 import { useForm } from 'react-hook-form'
 import FormItem from '../../../../components/form-item'
+import useRouterQuery from '../../../../components/use-router-query'
+import useArweaveFile from '../../../../hooks/use-arweave-file'
+import useDidConfig from '../../../../hooks/use-did-config'
 import { Proposal } from '../../../../src/schemas'
 
 export default function CreateProposalPage() {
   const { register } = useForm<Proposal>({ defaultValues: {} })
+  const [query] = useRouterQuery<['organization', 'workgroup']>()
+  const { data: config } = useDidConfig(query.organization)
+  const { data: organization } = useArweaveFile(config?.organization)
 
   return (
     <form>
