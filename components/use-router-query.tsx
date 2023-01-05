798a2b04fc5cc92d58dbabd85d5ab8ed2202d08a
Aliez <renzholy@gmail.com>
2023-01-05 17:36:15 +0800

    Create use-router-query.tsx

---
+++ components/use-router-query.tsx
@@ -0,0 +1,28 @@
+import produce from 'immer'
+import { useRouter } from 'next/router'
+import { useMemo } from 'react'
+
+export default function useRouterQuery<S extends string[]>() {
+  const router = useRouter()
+  return useMemo(
+    () =>
+      [
+        router.query,
+        (key, value, shallow) => {
+          router.push(
+            produce(router, (draft) => {
+              draft.query[key] = value
+            }),
+            undefined,
+            { shallow },
+          )
+        },
+      ] as [
+        Partial<{
+          [key in S[number]]: string
+        }>,
+        (key: S[number], value?: string, shallow?: boolean) => void,
+      ],
+    [router],
+  )
+}
