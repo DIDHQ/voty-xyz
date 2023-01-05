0728b035d3b06f6442046bc678a20f89eb6b1743
Aliez <renzholy@gmail.com>
2023-01-05 17:11:40 +0800

    Update create.tsx

--- pages/[organization]/workgroups/[workgroup]/create.tsx
+++ pages/[organization]/workgroups/[workgroup]/create.tsx
@@ -1,12 +1,22 @@
+import { Input, Textarea } from 'react-daisyui'
 import { useForm } from 'react-hook-form'
 import FormItem from '../../../../components/form-item'
+import { Proposal } from '../../../../src/schemas'
 
 export default function CreateProposalPage() {
-  const {} = useForm()
+  const { register } = useForm<Proposal>({ defaultValues: {} })
 
   return (
     <form>
-      <FormItem></FormItem>
+      <FormItem label="title">
+        <Input {...register('title')} />
+      </FormItem>
+      <FormItem label="body">
+        <Textarea {...register('body')} />
+      </FormItem>
+      <FormItem label="">
+        <Input {...register('discussion')} />
+      </FormItem>
     </form>
   )
 }
