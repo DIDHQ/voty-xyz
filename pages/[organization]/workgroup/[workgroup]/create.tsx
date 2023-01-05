a3d5743ed660c179a4a37cec68a4df9edb992d72
Aliez <renzholy@gmail.com>
2023-01-05 19:55:41 +0800

    Update create.tsx

--- pages/[organization]/workgroup/[workgroup]/create.tsx
+++ pages/[organization]/workgroup/[workgroup]/create.tsx
@@ -1,28 +1,46 @@
+import { useEffect } from 'react'
 import { Input, Textarea } from 'react-daisyui'
 import { useForm } from 'react-hook-form'
 import FormItem from '../../../../components/form-item'
 import useRouterQuery from '../../../../components/use-router-query'
 import useArweaveFile from '../../../../hooks/use-arweave-file'
 import useDidConfig from '../../../../hooks/use-did-config'
-import { Proposal } from '../../../../src/schemas'
+import { Organization, Proposal } from '../../../../src/schemas'
 
 export default function CreateProposalPage() {
-  const { register } = useForm<Proposal>({ defaultValues: {} })
+  const { register, setValue } = useForm<Proposal>({ defaultValues: {} })
   const [query] = useRouterQuery<['organization', 'workgroup']>()
   const { data: config } = useDidConfig(query.organization)
-  const { data: organization } = useArweaveFile(config?.organization)
+  const { data: organization } = useArweaveFile<Organization>(
+    config?.organization,
+  )
+  useEffect(() => {
+    if (!config?.organization) {
+      return
+    }
+    setValue('organization', config?.organization)
+  }, [config?.organization, setValue])
+  useEffect(() => {
+    const workgroup = organization?.workgroups?.find(
+      ({ profile }) => profile.name === query.workgroup,
+    )
+    if (!workgroup) {
+      return
+    }
+    setValue('workgroup', workgroup.id)
+  }, [organization?.workgroups, query.workgroup, setValue])
 
   return (
-    <form>
+    <>
       <FormItem label="title">
         <Input {...register('title')} />
       </FormItem>
       <FormItem label="body">
         <Textarea {...register('body')} />
       </FormItem>
-      <FormItem label="">
+      <FormItem label="discussion">
         <Input {...register('discussion')} />
       </FormItem>
-    </form>
+    </>
   )
 }
