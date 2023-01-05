e2a9d04c1724478a4b8dd7d79c49c00e3ceb52d2
mozwell <59408225+mozwell@users.noreply.github.com>
2023-01-05 16:09:54 +0800

    Feat/org (#15)
    * feat:add create org page
    
    * fix:resolve PR issues
    
    * fix:resolve PR issues again

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,4 +1,5 @@
 import { Navbar, Dropdown, Button } from 'react-daisyui'
+import Link from 'next/link'
 import dynamic from 'next/dynamic'
 import ThemeSwitcher from './theme-switcher'
 
@@ -14,16 +15,20 @@ const ConnectButtonCustom = dynamic(
 
 function NavBar(props: NavBarProps) {
   return (
-    <div className="pb-40 flex w-full component-preview p-4 items-center justify-center gap-2 font-sans">
+    <div className="flex w-full component-preview p-4 items-center justify-center gap-2 font-sans">
       <Navbar className="bg-base-200 shadow-xl rounded-box">
         <div className="flex-1">
-          <Button className="text-xl normal-case" color="ghost">
-            VotyXYZ
-          </Button>
+          <Link href="/">
+            <Button className="text-xl normal-case" color="ghost">
+              VotyXYZ
+            </Button>
+          </Link>
         </div>
         <div className="flex-none gap-3">
           <ThemeSwitcher />
-          <Button color="primary">Create an Organization</Button>
+          <Link href="/create">
+            <Button color="primary">Create an Organization</Button>
+          </Link>
           <ConnectButtonCustom>
             {({ account, openConnectModal }) => (
               <Button color="primary" onClick={openConnectModal}>
