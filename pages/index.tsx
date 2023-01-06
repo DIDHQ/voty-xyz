2e4b4d9613e36c7fcfeaf17936af52e93128810e
Aliez <renzholy@gmail.com>
2023-01-06 13:46:45 +0800

    add footer to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link'
 
+import Footer from '../components/footer'
+
 export default function IndexPage() {
   return (
     <>
       <Link href="/test">test</Link>
       <br />
       <Link href="/ph0ng.bit/settings">settings</Link>
+      <Footer />
     </>
   )
 }
