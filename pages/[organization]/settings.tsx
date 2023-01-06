dc4d014ee051dd937233165b726290f2f87d36fe
Aliez <renzholy@gmail.com>
2023-01-06 11:27:35 +0800

    useArweaveData hook

--- pages/[organization]/settings.tsx
+++ pages/[organization]/settings.tsx
@@ -3,14 +3,15 @@ import { Breadcrumbs } from 'react-daisyui'
 
 import OrganizationForm from '../../components/organization-form'
 import useRouterQuery from '../../components/use-router-query'
-import useArweaveFile from '../../hooks/use-arweave-file'
+import useArweaveData from '../../hooks/use-arweave-data'
 import useDidConfig from '../../hooks/use-did-config'
-import { Organization } from '../../src/schemas'
+import { organizationWithSignatureSchema } from '../../src/schemas'
 
 export default function OrganizationSettingsPage() {
   const [query] = useRouterQuery<['organization']>()
   const { data: config } = useDidConfig(query.organization)
-  const { data: organization } = useArweaveFile<Organization>(
+  const { data: organization } = useArweaveData(
+    organizationWithSignatureSchema,
     config?.organization,
   )
 
--- pages/[organization]/workgroup/[workgroup]/index.tsx
+++ pages/[organization]/workgroup/[workgroup]/index.tsx
@@ -2,16 +2,18 @@ import { HoldInterface } from '@icon-park/react'
 import Link from 'next/link'
 import { useMemo } from 'react'
 import { Breadcrumbs, Menu } from 'react-daisyui'
+
 import AvatarInput from '../../../../components/avatar-input'
 import useRouterQuery from '../../../../components/use-router-query'
-import useArweaveFile from '../../../../hooks/use-arweave-file'
+import useArweaveData from '../../../../hooks/use-arweave-data'
 import useDidConfig from '../../../../hooks/use-did-config'
-import { Organization } from '../../../../src/schemas'
+import { organizationWithSignatureSchema } from '../../../../src/schemas'
 
 export default function WorkgroupPage() {
   const [query] = useRouterQuery<['organization', 'workgroup']>()
   const { data: config } = useDidConfig(query.organization)
-  const { data: organization } = useArweaveFile<Organization>(
+  const { data: organization } = useArweaveData(
+    organizationWithSignatureSchema,
     config?.organization,
   )
   const workgroup = useMemo(
