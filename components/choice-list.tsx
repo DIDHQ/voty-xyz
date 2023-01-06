eff06fc08091f5048998992a9ecb6a5f6d0e6468
mozwell <59408225+mozwell@users.noreply.github.com>
2023-01-06 16:13:44 +0800

    Feat/choice list (#22)
    * feat:add demo choice list
    
    * feat:choice list done

---
+++ components/choice-list.tsx
@@ -0,0 +1,208 @@
+import React, { useMemo, useState, useCallback } from 'react'
+import type { CSSProperties } from 'react'
+import { DndContext, DragEndEvent } from '@dnd-kit/core'
+import { SortableContext, arrayMove, useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import { nanoid } from 'nanoid'
+import { Input, Button } from 'react-daisyui'
+import { Drag } from '@icon-park/react'
+import clsx from 'clsx'
+import produce from 'immer'
+
+type ChoiceListItemProps = {
+  id: string
+  value: string
+  index: number
+  maxLength?: number
+  readOnly?: boolean
+  onChange: (id: string, text: string) => void
+  onDelete?: (id: string) => void
+  onAdd?: () => void
+}
+
+function ChoiceListItem(props: ChoiceListItemProps) {
+  const { id, value, index, maxLength, readOnly, onChange, onDelete, onAdd } =
+    props
+
+  const {
+    attributes,
+    isDragging,
+    listeners,
+    setNodeRef,
+    setActivatorNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id, disabled: readOnly })
+
+  const style: CSSProperties = useMemo(
+    () => ({
+      opacity: isDragging ? 0.4 : undefined,
+      transform: CSS.Translate.toString(transform),
+      transition,
+    }),
+    [isDragging, transform, transition],
+  )
+
+  const dragBtnCls = clsx({
+    'absolute left-1 top-1/4': true,
+    'cursor-not-allowed': readOnly,
+  })
+
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      onChange(id, e.target.value)
+    },
+    [id, onChange],
+  )
+
+  const handleDelete = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      onDelete && onDelete(id)
+    },
+    [id, onDelete],
+  )
+
+  const handleAdd = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      onAdd && onAdd()
+    },
+    [onAdd],
+  )
+
+  return (
+    <div className="flex">
+      <div
+        className="relative mb-3 w-96"
+        ref={setNodeRef}
+        style={style}
+        key={id}
+      >
+        <Input
+          className="pl-24 pr-16 w-full placeholder:opacity-50"
+          value={value}
+          onChange={handleChange}
+          maxLength={maxLength}
+          placeholder={index > 0 ? '(Optional)' : undefined}
+          disabled={readOnly}
+        />
+        <button
+          className={dragBtnCls}
+          {...attributes}
+          {...listeners}
+          ref={setActivatorNodeRef}
+        >
+          <Drag size="1rem" />
+        </button>
+        <span className="opacity-50 absolute left-6 top-1/4 pointer-events-none">
+          Choice {index + 1}
+        </span>
+        {maxLength && (
+          <span className="opacity-50 absolute right-5 top-1/4 pointer-events-none">
+            {value.length}/{maxLength}
+          </span>
+        )}
+      </div>
+      {!readOnly && onDelete && (
+        <Button className="ml-3" onClick={handleDelete}>
+          -
+        </Button>
+      )}
+      {!readOnly && onAdd && (
+        <Button className="ml-3" onClick={handleAdd}>
+          +
+        </Button>
+      )}
+    </div>
+  )
+}
+
+export type ChoiceListProps = {
+  readOnly?: boolean
+  defaultChoices?: Array<string>
+  onChoicesChange: (choices: Array<string>) => void
+  maxLength?: number
+}
+
+export default function ChoiceList(props: ChoiceListProps) {
+  const { defaultChoices = [''], maxLength, onChoicesChange, readOnly } = props
+
+  const choiceData = useMemo(
+    () =>
+      defaultChoices.map((choice) => ({
+        id: nanoid(),
+        text: choice,
+      })),
+    [defaultChoices],
+  )
+
+  const [choices, setChoices] = useState(choiceData)
+
+  const changeChoices = useCallback(
+    (type: 'add' | 'delete' | 'modify', id?: string, text?: string) => {
+      const newChoices = produce(choices, (draft) => {
+        if (type === 'add') {
+          draft.push({ id: nanoid(), text: '' })
+          return
+        }
+        const targetIndex = draft.findIndex((choice) => choice.id === id)
+        if (type === 'delete') {
+          draft.splice(targetIndex, 1)
+        } else {
+          draft[targetIndex].text = text || ''
+        }
+      })
+      setChoices(newChoices)
+      onChoicesChange(newChoices.map((newChoice) => newChoice.text))
+    },
+    [choices, onChoicesChange],
+  )
+
+  const handleChange = useCallback(
+    (id: string, text: string) => {
+      changeChoices('modify', id, text)
+    },
+    [changeChoices],
+  )
+
+  const handleDelete = useCallback(
+    (id: string) => {
+      changeChoices('delete', id)
+    },
+    [changeChoices],
+  )
+
+  const handleAdd = useCallback(() => {
+    changeChoices('add')
+  }, [changeChoices])
+
+  const handleDragEnd = useCallback(
+    (dragEndProps: DragEndEvent) => {
+      const { active, over } = dragEndProps
+      const activeIndex = choices.findIndex((choice) => choice.id === active.id)
+      const overIndex = choices.findIndex((choice) => choice.id === over?.id)
+      const newChoices = arrayMove(choices, activeIndex, overIndex)
+      setChoices(newChoices)
+    },
+    [choices],
+  )
+
+  return (
+    <DndContext onDragEnd={handleDragEnd}>
+      <SortableContext items={choices}>
+        {choices.map((choice, index) => (
+          <ChoiceListItem
+            key={choice.id}
+            id={choice.id}
+            value={choice.text}
+            onChange={handleChange}
+            index={index}
+            maxLength={maxLength}
+            readOnly={readOnly}
+            onDelete={index < choices.length - 1 ? handleDelete : undefined}
+            onAdd={index === choices.length - 1 ? handleAdd : undefined}
+          />
+        ))}
+      </SortableContext>
+    </DndContext>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,26 @@
 import Link from 'next/link'
+import ChoiceList from '../components/choice-list'
 
 export default function IndexPage() {
+  const handleChoicesChange = (choices: string[]) => {
+    console.log('handleChoiceChange', choices)
+  }
   return (
     <>
       <Link href="/test">test</Link>
       <br />
+      <ChoiceList
+        // readOnly
+        onChoicesChange={handleChoicesChange}
+        maxLength={32}
+        defaultChoices={[
+          'I have a dream',
+          'Doraemon',
+          'Beautiful DND',
+          'Hello guys',
+          'Doraemon',
+        ]}
+      />
       <Link href="/ph0ng.bit/settings">settings</Link>
     </>
   )
