e591d90dac28795ac13d75f82e1c3cc285721b80
Aliez <renzholy@gmail.com>
2023-01-07 14:56:42 +0800

    add vote schema

---
+++ src/schemas/vote.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod'
+
+import { signatureSchema } from './signature'
+
+export const voteSchema = z.object({
+  organization: z.string().min(1),
+  workgroup: z.string().min(1),
+  proposal: z.string().min(1),
+  choice: z.union([z.number(), z.array(z.number())]),
+})
+export type Vote = z.infer<typeof voteSchema>
+
+export const voteWithSignatureSchema = voteSchema.extend({
+  signature: signatureSchema,
+})
+export type VoteWithSignature = z.infer<typeof voteWithSignatureSchema>
