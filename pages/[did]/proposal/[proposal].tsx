8600a41138d5f1ce96945bf5d7adb5eb0d35a764
Aliez <renzholy@gmail.com>
2023-01-08 11:56:38 +0800

    Update [proposal].tsx

--- pages/[did]/proposal/[proposal].tsx
+++ pages/[did]/proposal/[proposal].tsx
@@ -1,3 +1,5 @@
+import { ReactNode, useId, useState } from 'react'
+
 import useArweaveData from '../../../hooks/use-arweave-data'
 import useRouterQuery from '../../../hooks/use-router-query'
 import { proposalWithSignatureSchema } from '../../../src/schemas'
@@ -8,16 +10,52 @@ export default function ProposalPage() {
     proposalWithSignatureSchema,
     query.proposal,
   )
+  const [choice, setChoice] = useState('')
 
   return proposal ? (
     <>
       <h1>{proposal.title}</h1>
       <p>{proposal.body}</p>
-      <ul>
-        {proposal.choices.map((choice) => (
-          <li key={choice}>{choice}</li>
-        ))}
-      </ul>
+      {proposal.type === 'single' ? (
+        <>
+          {proposal.choices.map((c) => (
+            <Radio
+              key={c}
+              name="choice"
+              value={c === choice}
+              onChange={(e) => {
+                if (e) {
+                  setChoice(c)
+                }
+              }}
+            >
+              {c}
+            </Radio>
+          ))}
+        </>
+      ) : null}
     </>
   ) : null
+}
+
+function Radio(props: {
+  name: string
+  children: ReactNode
+  value: boolean
+  onChange(value: boolean): void
+}) {
+  const id = useId()
+
+  return (
+    <>
+      <input
+        type="radio"
+        id={id}
+        name={props.name}
+        checked={props.value}
+        onChange={(e) => props.onChange(e.target.checked)}
+      />
+      <label htmlFor={id}>{props.children}</label>
+    </>
+  )
 }
