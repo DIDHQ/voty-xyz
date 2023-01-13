d0fc3b7ea33f7215e71fb5a2a4f91aa7504d31ff
Aliez <renzholy@gmail.com>
2023-01-13 20:49:05 +0800

    add tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { IconProvider, DEFAULT_ICON_CONFIGS } from '@icon-park/react'
 import Head from 'next/head'
 import { injectGlobal } from '@emotion/css'
 import '@icon-park/react/styles/index.css'
+import '../styles/globals.css'
 
 import Layout from '../components/layout'
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,9 @@ export default function IndexPage() {
   return (
     <>
       <Link href="/create">
-        <button>Create an Organization</button>
+        <button className="inline-flex items-center rounded border border-transparent bg-indigo-600 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+          Create an Organization
+        </button>
       </Link>
       <ul>
         {organizations?.map((organization) => (
