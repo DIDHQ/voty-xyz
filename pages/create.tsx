14026542cad063cae186b3175bacf980e0893dd8
Aliez <renzholy@gmail.com>
2023-01-15 00:29:26 +0800

    Update create.tsx

--- pages/create.tsx
+++ pages/create.tsx
@@ -5,7 +5,6 @@ import { useEffect, useState } from 'react'
 import Alert from '../components/basic/alert'
 import FormItem from '../components/basic/form-item'
 import DidSelect from '../components/did-select'
-import OrganizationForm from '../components/organization-form'
 import useDidConfig from '../hooks/use-did-config'
 import useDids from '../hooks/use-dids'
 import useWallet from '../hooks/use-wallet'
@@ -20,24 +19,35 @@ export default function CreateOrganizationPage() {
   const { data: config } = useDidConfig(did)
 
   return (
-    <div className="p-8 space-y-8 divide-y divide-gray-200">
-      <div>
-        <FormItem
-          label="DID"
-          description="select one DID as your organization entry"
-        >
-          <DidSelect account={account} value={did} onChange={setDid} />
-        </FormItem>
-        {config?.organization ? (
-          <Alert
-            type="info"
-            text={`${did} already bounded to an organization`}
-            action={{ text: 'View', href: `/${did}` }}
-            className="mt-6"
-          />
-        ) : null}
-      </div>
-      {config?.organization ? null : <OrganizationForm did={did} />}
+    <div className="p-8">
+      <FormItem
+        label="DID"
+        description="select one DID as your organization entry"
+      >
+        <DidSelect account={account} value={did} onChange={setDid} />
+      </FormItem>
+      {dids?.length === 0 ? (
+        <Alert
+          type="info"
+          text={`Do not have a DID? Register one!`}
+          action={{ text: 'Register', href: 'https://app.did.id/explorer' }}
+          className="mt-6"
+        />
+      ) : config?.organization ? (
+        <Alert
+          type="warning"
+          text={`${did} already bounded to an organization`}
+          action={{ text: 'View', href: `/${did}` }}
+          className="mt-6"
+        />
+      ) : (
+        <Alert
+          type="success"
+          text={`${did} is able to create an organization`}
+          action={{ text: 'Create', href: `/${did}/settings` }}
+          className="mt-6"
+        />
+      )}
     </div>
   )
 }
