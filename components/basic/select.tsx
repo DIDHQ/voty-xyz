0a58978af7d9f83120f7ab426b54d4ae967d5092
Aliez <renzholy@gmail.com>
2023-01-15 00:17:04 +0800

    Update select.tsx

--- components/basic/select.tsx
+++ components/basic/select.tsx
@@ -14,7 +14,7 @@ export default function Select(props: {
         <>
           <div className="relative">
             <Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
-              <span className="block truncate">{props.value}</span>
+              <span className="block truncate min-h-[20px]">{props.value}</span>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <ChevronUpDownIcon
                   className="h-5 w-5 text-gray-400"
