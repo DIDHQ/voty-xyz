fa6b1c53b691291d83b8f036528c9e851e6f344f
Aliez <renzholy@gmail.com>
2023-01-14 16:27:28 +0800

    update

--- components/basic/avatar.tsx
+++ components/basic/avatar.tsx
@@ -1,20 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
 
 import BoringAvatar from 'boring-avatars'
+import clsx from 'clsx'
 
 export default function Avatar(props: {
   size: number
   name?: string
   value?: string
+  className?: string
 }) {
   return props.value ? (
     <img
       src={props.value}
       alt={props.name}
-      className={`w-${props.size} h-${props.size} object-cover rounded-full`}
+      className={clsx(
+        `w-${props.size} h-${props.size} object-cover rounded-full`,
+        props.className,
+      )}
     />
   ) : (
-    <div className="rounded-full overflow-hidden">
+    <div className={clsx('rounded-full overflow-hidden', props.className)}>
       <BoringAvatar size={props.size * 4} name={props.name} />
     </div>
   )
--- pages/[did]/index.tsx
+++ pages/[did]/index.tsx
@@ -1,14 +1,4 @@
 import Link from 'next/link'
-import {
-  Earth,
-  Twitter,
-  RobotOne,
-  GithubOne,
-  UserToUserTransmission,
-  Info,
-  SettingOne,
-  NetworkTree,
-} from '@icon-park/react'
 
 import Avatar from '../../components/basic/avatar'
 import useArweaveData from '../../hooks/use-arweave-data'
@@ -20,7 +10,13 @@ import {
 import useRouterQuery from '../../hooks/use-router-query'
 import { useList } from '../../hooks/use-api'
 import { DataType } from '../../src/constants'
-import ArweaveLink from '../../components/arweave-link'
+import PrimaryButton from '../../components/basic/primary-button'
+
+const stats = [
+  { label: 'Workgroups', value: 2 },
+  { label: 'Members', value: 12 },
+  { label: 'Proposals', value: 4 },
+]
 
 export default function OrganizationIndexPage() {
   const [query] = useRouterQuery<['did']>()
@@ -35,88 +31,45 @@ export default function OrganizationIndexPage() {
   )
 
   return organization ? (
-    <>
-      <Avatar
-        size={24}
-        name={organization.profile.name}
-        value={organization.profile.avatar}
-      />
-      {config?.organization ? <ArweaveLink id={config.organization} /> : null}
-      <h1>{organization.profile.name}</h1>
-      <div className="menu bg-base-100 w-56 rounded-box">
-        <ul>
-          <li>
-            <Link href={`/${query.did}`} className="active">
-              <NetworkTree />
-              Workgroups
+    <div className="overflow-hidden rounded-lg bg-white shadow m-8">
+      <div className="bg-white p-6">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <div className="sm:flex sm:space-x-5">
+            <div className="flex-shrink-0">
+              <Avatar
+                size={20}
+                name={organization.did}
+                value={organization.profile.avatar}
+                className="mx-auto"
+              />
+            </div>
+            <div className="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
+              <p className="text-xl font-bold text-gray-900 sm:text-2xl">
+                {organization.profile.name}
+              </p>
+              <p className="text-sm font-medium text-gray-600">
+                {organization.profile.about}
+              </p>
+            </div>
+          </div>
+          <div className="mt-5 flex justify-center sm:mt-0">
+            <Link href={`/${organization.did}/settings`}>
+              <PrimaryButton>Settings</PrimaryButton>
             </Link>
-          </li>
-          {organization.workgroups?.map((workgroup) => (
-            <li key={workgroup.id} className="ml-6">
-              <Link href={`/${query.did}/workgroup/${workgroup.profile.name}`}>
-                {workgroup.profile.name}
-              </Link>
-            </li>
-          ))}
-          <li>
-            <Link href={`/delegate/${query.did}`}>
-              <UserToUserTransmission />
-              Delegate
-            </Link>
-          </li>
-          <li>
-            <Link href={`/${query.did}/about`}>
-              <Info />
-              About
-            </Link>
-          </li>
-          <li>
-            <Link href={`/${query.did}/settings`}>
-              <SettingOne />
-              Settings
-            </Link>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
-      <div>
-        {organization.profile.website ? (
-          <button>
-            <a href={organization.profile.website}>
-              <Earth />
-            </a>
-          </button>
-        ) : null}
-        {organization.social?.twitter ? (
-          <button>
-            <a href={`https://twitter.com/${organization.social.twitter}`}>
-              <Twitter />
-            </a>
-          </button>
-        ) : null}
-        {organization.social?.discord ? (
-          <button>
-            <a href={`https://discord.gg/${organization.social.discord}`}>
-              <RobotOne />
-            </a>
-          </button>
-        ) : null}
-        {organization.social?.github ? (
-          <button>
-            <a href={`https://github.com/${organization.social.github}`}>
-              <GithubOne />
-            </a>
-          </button>
-        ) : null}
-      </div>
-      <ul>
-        {proposals?.map((proposal) => (
-          <li key={proposal.id}>
-            <Link href={`/${query.did}/proposal/${proposal.id}`}>
-              {proposal.title}
-            </Link>
-          </li>
+      <div className="grid grid-cols-1 divide-y divide-gray-200 border-t border-gray-200 bg-gray-50 sm:grid-cols-3 sm:divide-y-0 sm:divide-x">
+        {stats.map((stat) => (
+          <div
+            key={stat.label}
+            className="px-6 py-5 text-center text-sm font-medium"
+          >
+            <span className="text-gray-900">{stat.value}</span>{' '}
+            <span className="text-gray-600">{stat.label}</span>
+          </div>
         ))}
-      </ul>
-    </>
+      </div>
+    </div>
   ) : null
 }
