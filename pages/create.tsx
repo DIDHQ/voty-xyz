c8b63ed2bcb8b410da3fcf36b3cd9b966b1c00eb
Aliez <renzholy@gmail.com>
2023-01-15 00:13:43 +0800

    Alert

--- pages/create.tsx
+++ pages/create.tsx
@@ -2,9 +2,11 @@
 
 import { useEffect, useState } from 'react'
 
+import Alert from '../components/basic/alert'
 import FormItem from '../components/basic/form-item'
 import DidSelect from '../components/did-select'
 import OrganizationForm from '../components/organization-form'
+import useDidConfig from '../hooks/use-did-config'
 import useDids from '../hooks/use-dids'
 import useWallet from '../hooks/use-wallet'
 
@@ -15,13 +17,27 @@ export default function CreateOrganizationPage() {
   useEffect(() => {
     setDid(dids?.[0] || '')
   }, [dids])
+  const { data: config } = useDidConfig(did)
 
   return (
     <div className="p-8 space-y-8 divide-y divide-gray-200">
-      <FormItem label="DID">
-        <DidSelect account={account} value={did} onChange={setDid} />
-      </FormItem>
-      <OrganizationForm did={did} />
+      <div>
+        <FormItem
+          label="DID"
+          description="select one DID as your organization entry"
+        >
+          <DidSelect account={account} value={did} onChange={setDid} />
+        </FormItem>
+        {config?.organization ? (
+          <Alert
+            type="info"
+            text={`${did} already bounded to an organization`}
+            action={{ text: 'View', href: `/${did}` }}
+            className="mt-6"
+          />
+        ) : null}
+      </div>
+      {config?.organization ? null : <OrganizationForm did={did} />}
     </div>
   )
 }
