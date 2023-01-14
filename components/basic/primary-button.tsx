4abdc03e1f0ec95a146151a89f284fc844b9f41c
Aliez <renzholy@gmail.com>
2023-01-14 15:58:30 +0800

    update

--- components/basic/primary-button.tsx
+++ components/basic/primary-button.tsx
@@ -11,7 +11,7 @@ export default function PrimaryButton(
       {...restProps}
       disabled={loading || disabled}
       className={clsx(
-        'inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2',
+        'items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2',
         disabled
           ? 'cursor-not-allowed bg-gray-400 hover:bg-gray-400'
           : loading
