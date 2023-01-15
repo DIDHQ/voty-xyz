5e026c5b9ba592a15fa73f0421da1f6c86658a56
Aliez <renzholy@gmail.com>
2023-01-15 22:11:07 +0800

    update workgroup form

--- pages/[did]/settings.tsx
+++ pages/[did]/settings.tsx
@@ -2,8 +2,8 @@ import OrganizationForm from '../../components/organization-form'
 import useRouterQuery from '../../hooks/use-router-query'
 import useArweaveData from '../../hooks/use-arweave-data'
 import useDidConfig from '../../hooks/use-did-config'
-import { organizationWithSignatureSchema } from '../../src/schemas'
 import WorkgroupForm from '../../components/workgroup-form'
+import { organizationWithSignatureSchema } from '../../src/schemas'
 
 export default function OrganizationSettingsPage() {
   const [query] = useRouterQuery<['did', 'workgroup']>()
@@ -17,10 +17,12 @@ export default function OrganizationSettingsPage() {
     <div className="flex justify-center mb-8">
       <div className="flex flex-col w-full px-8">
         {query.workgroup ? (
-          <WorkgroupForm
-            organization={organization}
-            workgroup={query.workgroup}
-          />
+          organization ? (
+            <WorkgroupForm
+              organization={organization}
+              workgroup={query.workgroup}
+            />
+          ) : null
         ) : (
           <OrganizationForm did={query.did} organization={organization} />
         )}
