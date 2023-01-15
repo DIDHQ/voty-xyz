1429d96120371db658e5c447d868088483471570
Aliez <renzholy@gmail.com>
2023-01-16 00:08:09 +0800

    Update avatar.tsx

--- components/basic/avatar.tsx
+++ components/basic/avatar.tsx
@@ -13,7 +13,7 @@ export default function Avatar(props: {
       alt={props.name}
       width={props.size * 4}
       height={props.size * 4}
-      style={{ maxWidth: 'inherit', height: 'inherit' }}
+      style={{ width: props.size * 4, height: props.size * 4 }}
       className={clsx('object-cover rounded-full', props.className)}
     />
   ) : (
