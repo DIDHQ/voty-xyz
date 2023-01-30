b57453512d3153475ba7fef1ab2118e9e3af0983
Aliez <renzholy@gmail.com>
2023-01-30 17:40:10 +0800

    list votes

--- hooks/use-api.ts
+++ hooks/use-api.ts
@@ -1,26 +1,7 @@
-import useSWR from 'swr'
 import useSWRInfinite from 'swr/infinite'
 
-import { DataType } from '../src/constants'
-import { Authorized, Community, Proposal } from '../src/schemas'
+import { Authorized, Community, Proposal, Vote } from '../src/schemas'
 import { fetchJson } from '../src/utils/fetcher'
-
-export function useList<T>(
-  type: DataType,
-  where: [string, string | undefined][] = [],
-) {
-  return useSWR(['list', type, where], async () => {
-    const { data } = await fetchJson<{
-      data: ({ data: T } & { id: string })[]
-    }>(
-      `/api/list?${new URLSearchParams([
-        ['type', type],
-        ...(where.filter(([_key, value]) => value) as [string, string][]),
-      ]).toString()}`,
-    )
-    return data.map((item) => ({ ...item.data, id: item.id }))
-  })
-}
 
 export function useListCommunities() {
   return useSWRInfinite<{
@@ -46,6 +27,21 @@ export function useListProposals(entry?: string, group?: string) {
         `/api/list/proposals?entry=${entry}&group=${group || ''}&next=${
           next || ''
         }`,
+      )
+    },
+  )
+}
+
+export function useListVotes(proposal?: string) {
+  return useSWRInfinite<{
+    data: (Authorized<Vote> & { id: string })[]
+    next?: string
+  }>(
+    (_pageIndex, previousPageData) =>
+      proposal ? [proposal, previousPageData?.next] : null,
+    ([proposal, next]) => {
+      return fetchJson(
+        `/api/list/votes?proposal=${proposal}&next=${next || ''}`,
       )
     },
   )
