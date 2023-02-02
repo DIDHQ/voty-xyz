a0a1b4e124caaf21f4700698c68132562d22b790
Aliez <renzholy@gmail.com>
2023-02-02 18:26:29 +0800

    Update duration-input.tsx

--- components/basic/duration-input.tsx
+++ components/basic/duration-input.tsx
@@ -25,8 +25,8 @@ export default function DurationInput(props: {
       <div className="absolute inset-y-0 right-0 flex items-center">
         <select className="h-full rounded-md border-transparent bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
           <option>Hours</option>
-          <option>Weeks</option>
           <option>Days</option>
+          <option>Weeks</option>
           <option>Months</option>
           <option>Years</option>
         </select>
