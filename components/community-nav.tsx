f68c8d305c7f7a4d897e02d36e97ad97c88af4f8
Aliez <renzholy@gmail.com>
2023-02-08 22:45:09 +0800

    Update community-nav.tsx

--- components/community-nav.tsx
+++ components/community-nav.tsx
@@ -85,7 +85,7 @@ export default function CommunityNav(props: { className?: string }) {
     <aside className={props.className}>
       <div className="flex w-60 flex-col items-center rounded-md border border-gray-300 pb-4 shadow-sm">
         <Avatar
-          name={community?.author.did}
+          name={query.entry}
           value={community?.extension?.avatar}
           size={20}
           className="mt-8"
@@ -93,88 +93,96 @@ export default function CommunityNav(props: { className?: string }) {
         <h3 className="my-4 text-xl font-bold text-gray-900 sm:text-2xl">
           {community?.name || '...'}
         </h3>
-        <div className="mt-4 w-full space-y-1">
-          <h3
-            className="px-3 text-sm font-medium text-gray-400"
-            id="projects-headline"
-          >
-            Community
-          </h3>
-          {navigation.map((item) => (
-            <Link
-              key={item.name}
-              href={item.href}
-              className={clsx(
-                item.current
-                  ? 'border-indigo-600 bg-indigo-50 text-indigo-600'
-                  : 'border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                'group flex items-center border-l-4 px-3 py-2 text-sm font-medium',
-              )}
-            >
-              <item.icon
-                className={clsx(
-                  item.current
-                    ? 'text-indigo-500'
-                    : 'text-gray-400 group-hover:text-gray-500',
-                  'mr-3 h-6 w-6 shrink-0',
-                )}
-                aria-hidden="true"
-              />
-              {item.name}
-            </Link>
-          ))}
-        </div>
-        <div className="mt-4 w-full">
-          <h3
-            className="px-3 text-sm font-medium text-gray-400"
-            id="projects-headline"
-          >
-            Groups
-            <Link
-              href={`/${entry}/${community?.groups?.length || 0}/settings`}
-              className="float-right"
-            >
-              <PlusIcon className="h-5 w-5 text-gray-400 hover:text-gray-500" />
-            </Link>
-          </h3>
-          <div className="mt-1 space-y-1" aria-labelledby="projects-headline">
-            {community?.groups?.map((group, index) => (
-              <Link
-                key={group.name}
-                href={`/${entry}/${index}`}
-                className={clsx(
-                  query.group === index.toString()
-                    ? 'border-indigo-600 bg-indigo-50 text-indigo-600'
-                    : 'border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                  'group flex items-center border-l-4 px-3 py-2 text-sm font-medium',
-                )}
+        {community ? (
+          <>
+            {' '}
+            <div className="mt-4 w-full space-y-1">
+              <h3
+                className="px-3 text-sm font-medium text-gray-400"
+                id="projects-headline"
               >
-                {group.permission.adding_option ? (
-                  <TrophyIcon
+                Community
+              </h3>
+              {navigation.map((item) => (
+                <Link
+                  key={item.name}
+                  href={item.href}
+                  className={clsx(
+                    item.current
+                      ? 'border-indigo-600 bg-indigo-50 text-indigo-600'
+                      : 'border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+                    'group flex items-center border-l-4 px-3 py-2 text-sm font-medium',
+                  )}
+                >
+                  <item.icon
                     className={clsx(
-                      query.group === index.toString()
+                      item.current
                         ? 'text-indigo-500'
                         : 'text-gray-400 group-hover:text-gray-500',
                       'mr-3 h-6 w-6 shrink-0',
                     )}
                     aria-hidden="true"
                   />
-                ) : (
-                  <UserGroupIcon
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+            <div className="mt-4 w-full">
+              <h3
+                className="px-3 text-sm font-medium text-gray-400"
+                id="projects-headline"
+              >
+                Groups
+                <Link
+                  href={`/${entry}/${community?.groups?.length || 0}/settings`}
+                  className="float-right"
+                >
+                  <PlusIcon className="h-5 w-5 text-gray-400 hover:text-gray-500" />
+                </Link>
+              </h3>
+              <div
+                className="mt-1 space-y-1"
+                aria-labelledby="projects-headline"
+              >
+                {community?.groups?.map((group, index) => (
+                  <Link
+                    key={group.name}
+                    href={`/${entry}/${index}`}
                     className={clsx(
                       query.group === index.toString()
-                        ? 'text-indigo-500'
-                        : 'text-gray-400 group-hover:text-gray-500',
-                      'mr-3 h-6 w-6 shrink-0',
+                        ? 'border-indigo-600 bg-indigo-50 text-indigo-600'
+                        : 'border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+                      'group flex items-center border-l-4 px-3 py-2 text-sm font-medium',
                     )}
-                    aria-hidden="true"
-                  />
-                )}
-                <span className="truncate">{group.name}</span>
-              </Link>
-            ))}
-          </div>
-        </div>
+                  >
+                    {group.permission.adding_option ? (
+                      <TrophyIcon
+                        className={clsx(
+                          query.group === index.toString()
+                            ? 'text-indigo-500'
+                            : 'text-gray-400 group-hover:text-gray-500',
+                          'mr-3 h-6 w-6 shrink-0',
+                        )}
+                        aria-hidden="true"
+                      />
+                    ) : (
+                      <UserGroupIcon
+                        className={clsx(
+                          query.group === index.toString()
+                            ? 'text-indigo-500'
+                            : 'text-gray-400 group-hover:text-gray-500',
+                          'mr-3 h-6 w-6 shrink-0',
+                        )}
+                        aria-hidden="true"
+                      />
+                    )}
+                    <span className="truncate">{group.name}</span>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </>
+        ) : null}
       </div>
       <div className="my-4 flex space-x-4">
         {externals.map((item) => (
