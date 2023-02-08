7f431c855f69862402d20b211b32c9532b6ab982
Aliez <renzholy@gmail.com>
2023-02-08 14:22:46 +0800

    Update group.tsx

--- components/layouts/group.tsx
+++ components/layouts/group.tsx
@@ -5,10 +5,8 @@ import GroupNav from '../group-nav'
 export default function GroupLayout(props: { children: ReactNode }) {
   return (
     <div className="pl-6">
-      <GroupNav className="fixed top-16 z-40 pt-6" />
-      <div className="pt-28">
-        <div className="pt-8">{props.children}</div>
-      </div>
+      <GroupNav className="sticky top-16 z-40 pt-6" />
+      <div className="pt-6">{props.children}</div>
     </div>
   )
 }
