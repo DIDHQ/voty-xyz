285281d83827ebe3209ee9a9c3da4222d2c41407
Aliez <renzholy@gmail.com>
2023-02-10 00:41:38 +0800

    react-textarea-autosize

--- components/basic/textarea.tsx
+++ components/basic/textarea.tsx
@@ -1,17 +1,21 @@
 import clsx from 'clsx'
-import { forwardRef, TextareaHTMLAttributes } from 'react'
+import { forwardRef } from 'react'
+import TextareaAutosize, {
+  TextareaAutosizeProps,
+} from 'react-textarea-autosize'
 
 export default forwardRef<
   HTMLTextAreaElement,
-  TextareaHTMLAttributes<HTMLTextAreaElement> & { error?: boolean }
->(function TextInput(props, ref) {
+  TextareaAutosizeProps & { error?: boolean }
+>(function Textarea(props, ref) {
   const { error, children, className, ...restProps } = props
 
   return (
-    <textarea
+    <TextareaAutosize
       ref={ref}
       aria-invalid={error ? 'true' : 'false'}
       {...restProps}
+      minRows={3}
       className={clsx(
         'block h-24 w-full rounded-md border disabled:cursor-not-allowed disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 sm:text-sm',
         error
