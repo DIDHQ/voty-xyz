acc3dc599078f9558cac7b73288d557d8493e0a7
Aliez <renzholy@gmail.com>
2023-02-10 14:53:32 +0800

    Update create.tsx

--- pages/[entry]/[group]/create.tsx
+++ pages/[entry]/[group]/create.tsx
@@ -74,7 +74,7 @@ export default function CreateProposalPage() {
       : null,
     () => requiredCoinTypesOfNumberSets(group!.permission.voting!),
   )
-  const { data: snapshots } = useSWR(
+  const { data: snapshots, error } = useSWR(
     coinTypesOfNumberSets ? ['snapshots', coinTypesOfNumberSets] : null,
     async () => {
       const snapshots = await pMap(coinTypesOfNumberSets!, getCurrentSnapshot, {
@@ -107,6 +107,7 @@ export default function CreateProposalPage() {
       [handleUpload, handleSignJson],
     ),
   )
+  console.log('snapshots', snapshots, error)
 
   return (
     <div className="mt-6">
