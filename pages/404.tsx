aab17c2c01b23091b1164cda35a89a64c0a9e0b8
Aliez <renzholy@gmail.com>
2023-02-12 19:46:49 +0800

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,9 @@
+import Link from 'next/link'
+
 export default function NotFoundPage() {
   return (
-    <main className="mx-auto flex w-full max-w-7xl grow flex-col px-6 lg:px-8">
-      <div className="my-auto shrink-0 py-16 sm:py-32">
+    <main className="flex w-full grow flex-col">
+      <div className="my-auto shrink-0 py-16">
         <p className="text-base font-semibold text-indigo-600">404</p>
         <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
           Page not found
@@ -10,13 +12,13 @@ export default function NotFoundPage() {
           Sorry, we couldn’t find the page you’re looking for.
         </p>
         <div className="mt-6">
-          <a
-            href="#"
+          <Link
+            href="/"
             className="text-base font-medium text-indigo-600 hover:text-indigo-500"
           >
             Go back home
             <span aria-hidden="true"> &rarr;</span>
-          </a>
+          </Link>
         </div>
       </div>
     </main>
