0fd290c88553ad8bb4507dff3f4e7a4634e1fad7
Aliez <renzholy@gmail.com>
2023-02-13 19:31:03 +0800

    update

--- pages/[entry]/[group]/create.tsx
+++ pages/[entry]/[group]/create.tsx
@@ -116,7 +116,6 @@ export default function CreateProposalPage() {
       })),
     [],
   )
-  console.log(group?.extension.terms_and_conditions)
 
   return (
     <div className="mt-6 flex items-start">
@@ -240,7 +239,7 @@ export default function CreateProposalPage() {
             </DetailItem>
           </DetailList>
           <DetailList title="Terms and conditions">
-            <article className="prose-sm prose-ol:list-decimal marker:prose-ol:text-gray-400 prose-ul:list-disc marker:prose-ul:text-gray-400">
+            <article className="prose-sm pt-2 prose-ol:list-decimal marker:prose-ol:text-gray-400 prose-ul:list-disc marker:prose-ul:text-gray-400">
               <Markdown>{group?.extension.terms_and_conditions}</Markdown>
             </article>
           </DetailList>
