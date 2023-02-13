b63ff585eec9ae7fd2d4328c9ba5fb04fc79877d
Aliez <renzholy@gmail.com>
2023-02-13 19:39:02 +0800

    Update status.tsx

--- components/status.tsx
+++ components/status.tsx
@@ -1,4 +1,5 @@
 import { CubeIcon, CubeTransparentIcon } from '@heroicons/react/24/outline'
+import { useMemo } from 'react'
 
 import useStatus from '../hooks/use-status'
 import { permalink2Url } from '../src/arweave'
@@ -6,20 +7,21 @@ import TextButton from './basic/text-button'
 
 export default function Status(props: { permalink?: string }) {
   const { data: status } = useStatus(props.permalink)
-
-  return (
-    <TextButton
-      onClick={() => {
-        if (props.permalink) {
-          window.open(permalink2Url(props.permalink))
-        }
-      }}
-    >
-      {status?.timestamp ? (
+  const icon = useMemo(
+    () =>
+      status?.timestamp ? (
         <CubeIcon className="h-5 w-5" />
       ) : (
         <CubeTransparentIcon className="h-5 w-5" />
-      )}
-    </TextButton>
+      ),
+    [status?.timestamp],
+  )
+
+  return props.permalink ? (
+    <a href={permalink2Url(props.permalink)}>
+      <TextButton>{icon}</TextButton>
+    </a>
+  ) : (
+    <TextButton>{icon}</TextButton>
   )
 }
