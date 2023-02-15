8d5404683ed0f3ce5dfc9c351186bb55cadb9391
Aliez <renzholy@gmail.com>
2023-02-15 11:29:35 +0800

    Update index.tsx

--- src/pages/[entry]/[group]/[proposal]/index.tsx
+++ src/pages/[entry]/[group]/[proposal]/index.tsx
@@ -96,6 +96,7 @@ export default function ProposalPage() {
     refetchList()
     refetchChoices()
     setValue('choice', '')
+    setDid('')
   }, [refetchList, refetchChoices, setValue])
   const disabled = !did
 
