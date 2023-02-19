8124e0a6727128c1cc00e3466bb0bf7787d83eb8
Aliez <renzholy@gmail.com>
2023-02-19 15:55:10 +0800

    Update index.tsx

--- src/pages/[entry]/index.tsx
+++ src/pages/[entry]/index.tsx
@@ -1,9 +1,8 @@
-import { useMemo, useState } from 'react'
+import { useMemo } from 'react'
 
 import useRouterQuery from '../../hooks/use-router-query'
 import ProposalListItem from '../../components/proposal-list-item'
 import CommunityLayout from '../../components/layouts/community'
-import Select from '../../components/basic/select'
 import { trpc } from '../../utils/trpc'
 
 export default function CommunityIndexPage() {
@@ -15,18 +14,9 @@ export default function CommunityIndexPage() {
     () => list?.pages.flatMap(({ data }) => data),
     [list],
   )
-  const [order, setOrder] = useState('All')
 
   return (
     <CommunityLayout>
-      <div className="sticky top-16 flex w-full justify-between border-b border-gray-200 bg-white/80 py-6 pl-6 backdrop-blur">
-        <Select
-          options={['All', 'Active', 'Pending', 'Closed']}
-          value={order}
-          onChange={setOrder}
-          className="w-36 shrink-0"
-        />
-      </div>
       <ul role="list" className="divide-y divide-gray-200">
         {proposals?.map((proposal) => (
           <li key={proposal.permalink}>
