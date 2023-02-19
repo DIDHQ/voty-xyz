c358725fa256d7cbe093f7d92ef633f4361c1b86
Aliez <renzholy@gmail.com>
2023-02-19 20:21:47 +0800

    update disconnect

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,5 +1,8 @@
 import Link from 'next/link'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+import clsx from 'clsx'
 import '@rainbow-me/rainbowkit/styles.css'
 
 import Button from './basic/button'
@@ -9,7 +12,7 @@ import TextButton from './basic/text-button'
 import { isTestnet } from '../utils/testnet'
 
 export default function Toolbar(props: { className?: string }) {
-  const { account, avatar, name: did, displayAddress } = useWallet()
+  const { account, avatar, name: did, displayAddress, disconnect } = useWallet()
 
   return (
     <header className={props.className}>
@@ -24,8 +27,8 @@ export default function Toolbar(props: { className?: string }) {
         <ConnectButton.Custom>
           {({ openConnectModal, connectModalOpen }) =>
             account ? (
-              <Link href="/settings" className="group block shrink-0">
-                <div className="flex items-center overflow-hidden">
+              <Menu as="div" className="relative inline-block text-left">
+                <Menu.Button className="group flex shrink-0 items-center overflow-hidden">
                   <Avatar
                     size={9}
                     name={did || account.address}
@@ -40,8 +43,37 @@ export default function Toolbar(props: { className?: string }) {
                       {displayAddress}
                     </p>
                   </div>
-                </div>
-              </Link>
+                </Menu.Button>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                    <div className="py-1">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            onClick={disconnect}
+                            className={clsx(
+                              active
+                                ? 'bg-gray-100 text-gray-900'
+                                : 'text-gray-700',
+                              'block w-full px-4 py-2 text-left text-sm',
+                            )}
+                          >
+                            Disconnect
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
             ) : (
               <Button
                 primary
