e43845e68d906d9096b1c6cc38e6644fcb7b6be2
Aliez <renzholy@gmail.com>
2023-02-20 10:30:13 +0800

    Update community-card.tsx

--- src/components/community-card.tsx
+++ src/components/community-card.tsx
@@ -22,32 +22,34 @@ export default function CommunityCard(props: {
       href={`/${community.authorship.author}`}
       className="block border border-gray-200 p-4 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400 hover:bg-gray-50"
     >
-      <div className="flex justify-between">
+      <div className="flex items-start">
         <Avatar
           size={20}
           name={community.authorship.author}
           value={community.extension?.avatar}
         />
-        <div className="ml-4 space-y-2 text-sm text-gray-500">
-          <div className="flex items-center">
-            <UserGroupIcon className="mr-2 h-4 w-4" />
-            {community.workgroups?.length || 0}
-          </div>
-          <div className="flex items-center">
-            <HandRaisedIcon className="mr-2 h-4 w-4" />
-            {community.entry.proposals}
-          </div>
-          <div className="flex items-center">
-            <BookmarkIcon className="mr-2 h-4 w-4" />
-            {community.entry.subscribers}
-          </div>
+        <div className="ml-4">
+          <h3 className="text-lg font-medium text-gray-900">
+            {community.name}
+          </h3>
+          <p className="flex items-center text-gray-500">
+            <span className="truncate">{community.authorship.author}</span>
+          </p>
         </div>
       </div>
-      <div className="pt-4">
-        <h3 className="font-medium text-gray-900">{community.name}</h3>
-        <p className="flex items-center text-sm text-gray-500">
-          <span className="truncate">{community.authorship.author}</span>
-        </p>
+      <div className="mt-4 flex justify-between space-x-2 pr-10 text-sm text-gray-500">
+        <div className="flex flex-1 items-center">
+          <UserGroupIcon className="mr-2 h-4 w-4" />
+          {community.workgroups?.length || 0}
+        </div>
+        <div className="flex flex-1 items-center">
+          <HandRaisedIcon className="mr-2 h-4 w-4" />
+          {community.entry.proposals}
+        </div>
+        <div className="flex flex-1 items-center">
+          <BookmarkIcon className="mr-2 h-4 w-4" />
+          {community.entry.subscribers}
+        </div>
       </div>
     </Link>
   )
