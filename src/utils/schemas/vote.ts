b776546f08fc6c37db043f36d3acec2ebaa1d199
Aliez <renzholy@gmail.com>
2023-02-21 19:15:33 +0800

    Update vote.ts

--- src/utils/schemas/vote.ts
+++ src/utils/schemas/vote.ts
@@ -2,16 +2,15 @@ import { z } from 'zod'
 
 import { choiceIsEmpty } from '../voting'
 
-export const voteSchema = z
-  .object({
-    proposal: z.string().min(1),
-    choice: z.string(),
-    power: z.number(),
-  })
-  .refine(
-    (vote) =>
-      !choiceIsEmpty('single', vote.choice) &&
-      !choiceIsEmpty('multiple', vote.choice),
-  )
+export const voteSchema = z.object({
+  proposal: z.string().min(1),
+  choice: z
+    .string()
+    .refine(
+      (choice) =>
+        !choiceIsEmpty('single', choice) && !choiceIsEmpty('multiple', choice),
+    ),
+  power: z.number(),
+})
 
 export type Vote = z.infer<typeof voteSchema>
