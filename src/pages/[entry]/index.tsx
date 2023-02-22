40bc10a5fe4ffe1baed0abf05b4d186afeeed7ac
Aliez <renzholy@gmail.com>
2023-02-22 16:53:11 +0800

    Update index.tsx

--- src/pages/[entry]/index.tsx
+++ src/pages/[entry]/index.tsx
@@ -1,23 +1,34 @@
 import { useEffect, useMemo } from 'react'
 import { useInView } from 'react-intersection-observer'
+import Link from 'next/link'
+import { DocumentPlusIcon } from '@heroicons/react/20/solid'
 
 import useRouterQuery from '../../hooks/use-router-query'
 import ProposalListItem from '../../components/proposal-list-item'
 import CommunityLayout from '../../components/layouts/community'
 import { trpc } from '../../utils/trpc'
 import LoadingBar from '../../components/basic/loading-bar'
+import Button from '../../components/basic/button'
 
 export default function CommunityIndexPage() {
   const query = useRouterQuery<['entry']>()
-  const { data, fetchNextPage, hasNextPage, isLoading } =
-    trpc.proposal.list.useInfiniteQuery(
-      { entry: query.entry },
-      {
-        enabled: !!query.entry,
-        getNextPageParam: ({ next }) => next,
-        refetchOnWindowFocus: false,
-      },
-    )
+  const { data: community, isLoading } = trpc.community.getByEntry.useQuery(
+    { entry: query.entry },
+    { enabled: !!query.entry, refetchOnWindowFocus: false },
+  )
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading: isListLoading,
+  } = trpc.proposal.list.useInfiniteQuery(
+    { entry: query.entry },
+    {
+      enabled: !!query.entry,
+      getNextPageParam: ({ next }) => next,
+      refetchOnWindowFocus: false,
+    },
+  )
   const proposals = useMemo(
     () => data?.pages.flatMap(({ data }) => data),
     [data],
@@ -31,9 +42,27 @@ export default function CommunityIndexPage() {
 
   return (
     <CommunityLayout>
-      <LoadingBar loading={isLoading} />
+      <LoadingBar loading={isLoading || isListLoading} />
       {proposals?.length === 0 ? (
-        <p className="mt-6 text-sm text-gray-500 sm:pl-6">No events</p>
+        community?.workgroups?.length ? (
+          <p className="mt-6 text-sm text-gray-500 sm:pl-6">No events</p>
+        ) : (
+          <div className="mx-auto mt-6 py-20 text-center">
+            <h2 className="text-4xl font-bold tracking-tight text-gray-900">
+              Create workgroup
+            </h2>
+            <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600">
+              Workgroup is the hub of proposals
+            </p>
+            <div className="mt-10 flex items-center justify-center">
+              <Link href={`/${query.entry}/create`}>
+                <Button primary icon={DocumentPlusIcon}>
+                  Create
+                </Button>
+              </Link>
+            </div>
+          </div>
+        )
       ) : (
         <ul role="list" className="divide-y divide-gray-200 sm:pl-6">
           {proposals?.map((proposal) => (
