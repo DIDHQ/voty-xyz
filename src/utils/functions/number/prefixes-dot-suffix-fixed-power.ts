8732881f7a0dc502de4a14cb388423b60049b0a4
Aliez <renzholy@gmail.com>
2023-02-22 14:08:42 +0800

    Update prefixes-dot-suffix-fixed-power.ts

--- src/utils/functions/number/prefixes-dot-suffix-fixed-power.ts
+++ src/utils/functions/number/prefixes-dot-suffix-fixed-power.ts
@@ -3,16 +3,16 @@ import Decimal from 'decimal.js'
 import { NumberFunction } from '../../types'
 
 export const prefixes_dot_suffix_fixed_power: NumberFunction<
-  [string, string[], Decimal]
+  [string, string[], string]
 > = (suffix, prefixes, power) => {
   const set = new Set(prefixes.map((prefix) => `${prefix}.${suffix}`))
 
   return {
     requiredCoinTypes: [],
     execute: (did) => {
-      return (set.size ? set.has(did) : did.endsWith(`.${suffix}`))
-        ? power
-        : new Decimal(0)
+      return new Decimal(
+        (set.size ? set.has(did) : did.endsWith(`.${suffix}`)) ? power : 0,
+      )
     },
   }
 }
