2df524c3eedc9b157671b07493f2a94a14bc59b9
Aliez <renzholy@gmail.com>
2023-02-22 11:40:54 +0800

    Update index.ts

--- src/utils/functions/number/index.ts
+++ src/utils/functions/number/index.ts
@@ -1,5 +1,5 @@
 import Decimal from 'decimal.js'
-import { max, maxBy, sum, sumBy, uniq } from 'lodash-es'
+import { uniq } from 'lodash-es'
 import pMap from 'p-map'
 
 import { NumberSets, NumberUnit } from '../../schemas/sets'
