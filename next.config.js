65ee2922bfbbd4c06badd4299c7431035410a2f7
Aliez <renzholy@gmail.com>
2023-02-23 17:30:29 +0800

    tailwindcss ssr

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    optimizeCss: true,
+  },
   webpack(config) {
     return {
       ...config,
