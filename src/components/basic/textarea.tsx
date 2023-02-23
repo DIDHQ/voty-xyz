ecf6af0bddee5e223c382d8cf2e3a77d81c72284
Aliez <renzholy@gmail.com>
2023-02-23 13:33:00 +0800

    Update textarea.tsx

--- src/components/basic/textarea.tsx
+++ src/components/basic/textarea.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import { forwardRef } from 'react'
+import { forwardRef, KeyboardEvent, useCallback } from 'react'
 import TextareaAutosize, {
   TextareaAutosizeProps,
 } from 'react-textarea-autosize'
@@ -9,13 +9,26 @@ export default forwardRef<
   TextareaAutosizeProps & { error?: boolean }
 >(function Textarea(props, ref) {
   const { error, children, className, ...restProps } = props
+  const handleKeyDown = useCallback((e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key == 'Tab') {
+      e.preventDefault()
+      const start = e.currentTarget.selectionStart
+      const end = e.currentTarget.selectionEnd
+      e.currentTarget.value =
+        e.currentTarget.value.substring(0, start) +
+        '\t' +
+        e.currentTarget.value.substring(end)
+      e.currentTarget.selectionStart = e.currentTarget.selectionEnd = start + 1
+    }
+  }, [])
 
   return (
     <TextareaAutosize
       ref={ref}
       aria-invalid={error ? 'true' : 'false'}
-      {...restProps}
+      onKeyDown={handleKeyDown}
       minRows={3}
+      {...restProps}
       className={clsx(
         'block h-24 w-full border disabled:cursor-not-allowed disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500 sm:text-sm',
         error
