da198cd76ae36d455d6184f7e3f0456dbce8144a
Aliez <renzholy@gmail.com>
2023-02-23 16:57:12 +0800

    Update markdown.tsx

--- src/components/basic/markdown.tsx
+++ src/components/basic/markdown.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useMemo } from 'react'
 import {
   defaultRules,
   ParserRules,
@@ -6,44 +7,45 @@ import {
   outputFor,
 } from 'simple-markdown'
 
-const rules: ParserRules = {
-  ...defaultRules,
-  paragraph: {
-    ...defaultRules.paragraph,
-    react: (node, output, state) => {
-      return <p key={state.key}>{output((node as any).content, state)}</p>
-    },
-  },
-  link: {
-    ...defaultRules.link,
-    react: (node, output, state) => {
-      const href = (node as any).target
-      return typeof href === 'string' &&
-        (href.startsWith('/') || href.startsWith('#')) ? (
-        <Link
-          key={state.key}
-          href={href}
-          className="break-all text-primary-600 hover:text-primary-500"
-        >
-          {output((node as any).content, state)}
-        </Link>
-      ) : (
-        <a
-          key={state.key}
-          href={href}
-          className="break-all text-primary-600 hover:text-primary-500"
-        >
-          {output((node as any).content, state)}
-        </a>
-      )
-    },
-  },
-}
-
-const parser = parserFor(rules)
-
-const output = outputFor(rules, 'react' as any)
-
 export default function Markdown(props: { children: string }) {
+  const rules = useMemo<ParserRules>(
+    () => ({
+      ...defaultRules,
+      paragraph: {
+        ...defaultRules.paragraph,
+        react: (node, output, state) => {
+          return <p key={state.key}>{output((node as any).content, state)}</p>
+        },
+      },
+      link: {
+        ...defaultRules.link,
+        react: (node, output, state) => {
+          const href = (node as any).target
+          return typeof href === 'string' &&
+            (href.startsWith('/') || href.startsWith('#')) ? (
+            <Link
+              key={state.key}
+              href={href}
+              className="break-all text-primary-600 hover:text-primary-500"
+            >
+              {output((node as any).content, state)}
+            </Link>
+          ) : (
+            <a
+              key={state.key}
+              href={href}
+              className="break-all text-primary-600 hover:text-primary-500"
+            >
+              {output((node as any).content, state)}
+            </a>
+          )
+        },
+      },
+    }),
+    [],
+  )
+  const parser = useMemo(() => parserFor(rules), [rules])
+  const output = useMemo(() => outputFor(rules, 'react' as any), [rules])
+
   return <>{output(parser(`${props.children || ''}\n\n`, { inline: false }))}</>
 }
