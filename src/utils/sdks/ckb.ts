3f6f256fba566e22af68275aaa8882d19095da50
Aliez <renzholy@gmail.com>
2023-02-23 14:16:26 +0800

    Update ckb.ts

--- src/utils/sdks/ckb.ts
+++ src/utils/sdks/ckb.ts
@@ -3,7 +3,9 @@ import CKB from '@nervosnetwork/ckb-sdk-core'
 import { isTestnet } from '../constants'
 
 const ckb = new CKB(
-  isTestnet ? 'https://testnet.ckb.dev/' : 'https://mainnet.ckb.dev/',
+  isTestnet
+    ? 'https://test-node-api.did.id/node'
+    : 'https://node-api.did.id/node',
 )
 
 export default ckb
