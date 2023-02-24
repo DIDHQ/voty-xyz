03c5b70858f0fb48d5fffba3c35e42376115df7c
Aliez <renzholy@gmail.com>
2023-02-24 15:16:29 +0800

    Create error.tsx

---
+++ src/pages/error.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+
+export default function ErrorPage() {
+  return (
+    <main className="flex w-full grow flex-col">
+      <div className="my-auto shrink-0 py-16">
+        <p className="text-base font-semibold text-primary-600">404</p>
+        <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          Error
+        </h1>
+        <p className="mt-2 text-base text-gray-500">
+          A client-side exception has occurred
+        </p>
+        <div className="mt-6">
+          <Link
+            href="/"
+            className="text-base font-medium text-primary-600 hover:text-primary-500"
+          >
+            Go back home
+            <span aria-hidden="true"> &rarr;</span>
+          </Link>
+        </div>
+      </div>
+    </main>
+  )
+}
