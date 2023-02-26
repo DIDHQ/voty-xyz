9225c105aaa4b51424d78bfc68eec2e26df4d51d
Aliez <renzholy@gmail.com>
2023-02-26 23:55:46 +0800

    update connect button

--- src/components/connect-button.tsx
+++ src/components/connect-button.tsx
@@ -1,14 +1,16 @@
 import { ConnectButton as RainbowConnectButton } from '@rainbow-me/rainbowkit'
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+import clsx from 'clsx'
 import '@rainbow-me/rainbowkit/styles.css'
 
 import useWallet from '../hooks/use-wallet'
 import Button from './basic/button'
 import Avatar from './basic/avatar'
-import TextButton from './basic/text-button'
 import { chainIdToCoinType, coinTypeNames } from '../utils/constants'
 
 export default function ConnectButton() {
-  const { account, displayAddress } = useWallet()
+  const { account, displayAddress, disconnect } = useWallet()
 
   return (
     <RainbowConnectButton.Custom>
@@ -20,17 +22,48 @@ export default function ConnectButton() {
         chain,
       }) =>
         account ? (
-          <TextButton href="/settings" className="flex items-center">
-            <Avatar size={9} name={account.address} variant="beam" />
-            <div className="ml-2 hidden text-start sm:block">
-              <p className="text-start text-sm font-medium text-gray-700 group-hover:text-gray-900">
-                {coinTypeNames[account.coinType]}
-              </p>
-              <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">
-                {displayAddress}
-              </p>
-            </div>
-          </TextButton>
+          <Menu as="div" className="relative inline-block text-left">
+            <Menu.Button className="flex items-center">
+              <Avatar size={9} name={account.address} variant="beam" />
+              <div className="ml-2 hidden text-start sm:block">
+                <p className="text-start text-sm font-medium text-gray-700 group-hover:text-gray-900">
+                  {coinTypeNames[account.coinType]}
+                </p>
+                <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">
+                  {displayAddress}
+                </p>
+              </div>
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                <div className="py-1">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        onClick={disconnect}
+                        className={clsx(
+                          active
+                            ? 'bg-gray-100 text-gray-900'
+                            : 'text-gray-700',
+                          'block w-full px-4 py-2 text-start text-sm',
+                        )}
+                      >
+                        Disconnect
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
         ) : !chain || chainIdToCoinType[chain.id] ? (
           <Button primary loading={connectModalOpen} onClick={openConnectModal}>
             Connect Wallet
