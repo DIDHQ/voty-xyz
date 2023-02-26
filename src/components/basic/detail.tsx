697256cf450b27b7db5d2a3750012fee4b8990e4
Aliez <renzholy@gmail.com>
2023-02-26 18:45:37 +0800

    Update connect-button.tsx

--- src/components/basic/detail.tsx
+++ src/components/basic/detail.tsx
@@ -5,9 +5,7 @@ export function DetailList(props: { title: string; children: ReactNode }) {
   return (
     <div>
       <h3 className="font-medium text-gray-900">{props.title}</h3>
-      <dl className="mt-2 divide-y divide-gray-200 border-t">
-        {props.children}
-      </dl>
+      <dl className="mt-2 border-t">{props.children}</dl>
     </div>
   )
 }
