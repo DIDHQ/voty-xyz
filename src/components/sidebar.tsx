eef3b4569dc8341ad9b3728e9dc32b6452a476f1
Aliez <renzholy@gmail.com>
2023-02-26 23:23:16 +0800

    fix subscription list

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -26,7 +26,7 @@ export default function Sidebar(props: { className?: string }) {
             href="/create"
             data-tooltip-id={id}
             data-tooltip-place="right"
-            className="group my-3 flex h-12 w-12 shrink-0 items-center justify-center rounded-full border border-gray-200 hover:bg-primary-600"
+            className="group mt-2 mb-3 flex h-12 w-12 shrink-0 items-center justify-center rounded-full border border-gray-200 hover:bg-primary-600"
           >
             <PlusIcon className="h-8 w-8 text-primary-600 group-hover:text-white" />
           </Link>
--- src/components/subscription-list.tsx
+++ src/components/subscription-list.tsx
@@ -1,9 +1,12 @@
-import clsx from 'clsx'
 import { useAtomValue } from 'jotai'
 import Link from 'next/link'
+import { useId } from 'react'
+import { Tooltip } from 'react-tooltip'
 
 import useRouterQuery from '../hooks/use-router-query'
 import { currentDidAtom } from '../utils/atoms'
+import { Authorized } from '../utils/schemas/authorship'
+import { Community } from '../utils/schemas/community'
 import { trpc } from '../utils/trpc'
 import Avatar from './basic/avatar'
 
@@ -16,26 +19,48 @@ export default function SubscriptionList() {
   )
 
   return (
-    <div className="flex w-full flex-col items-center overflow-y-auto">
+    <div className="flex w-full flex-col items-center overflow-y-auto pb-1">
       {data?.map((community) => (
-        <Link
+        <SubscriptionListItem
           key={community.authorship.author}
-          href={`/${community.authorship.author}`}
-        >
-          <Avatar
-            size={12}
-            name={community.authorship.author}
-            value={community.extension?.avatar}
-            noRing
-            className={clsx(
-              'mt-3',
-              community.authorship.author === query.entry
-                ? 'ring-2 ring-primary-500 ring-offset-2'
-                : 'ring-1 ring-gray-200 hover:ring-2 hover:ring-gray-300 hover:ring-offset-2',
-            )}
-          />
-        </Link>
+          value={community}
+          selected={community.authorship.author === query.entry}
+        />
       ))}
     </div>
+  )
+}
+
+function SubscriptionListItem(props: {
+  value: Authorized<Community>
+  selected: boolean
+}) {
+  const id = useId()
+
+  return (
+    <>
+      <Link
+        key={props.value.authorship.author}
+        data-tooltip-id={id}
+        data-tooltip-place="right"
+        href={`/${props.value.authorship.author}`}
+        className="mt-3"
+      >
+        <Avatar
+          size={12}
+          name={props.value.authorship.author}
+          value={props.value.extension?.avatar}
+          noRing
+          className={
+            props.selected
+              ? 'ring-2 ring-primary-500 ring-offset-2'
+              : 'ring-1 ring-gray-200 hover:ring-2 hover:ring-gray-300 hover:ring-offset-2'
+          }
+        />
+      </Link>
+      <Tooltip id={id} className="rounded">
+        {props.value.name}
+      </Tooltip>
+    </>
   )
 }
