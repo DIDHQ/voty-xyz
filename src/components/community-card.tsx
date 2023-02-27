52ba2887e204ac7ebb5ab00fe23548e61f304a54
Aliez <renzholy@gmail.com>
2023-02-27 19:41:17 +0800

    Update community-card.tsx

--- src/components/community-card.tsx
+++ src/components/community-card.tsx
@@ -1,8 +1,4 @@
-import {
-  BookmarkIcon,
-  HandRaisedIcon,
-  BriefcaseIcon,
-} from '@heroicons/react/20/solid'
+import { BookmarkIcon } from '@heroicons/react/24/outline'
 import { Entry } from '@prisma/client'
 import { Serialize } from '@trpc/server/dist/shared/internal/serialize'
 import Link from 'next/link'
@@ -10,7 +6,6 @@ import Link from 'next/link'
 import { Authorized } from '../utils/schemas/authorship'
 import { Community } from '../utils/schemas/community'
 import Avatar from './basic/avatar'
-import { Grid6 } from './basic/grid'
 
 export default function CommunityCard(props: {
   community: Authorized<Community> & Serialize<{ entry: Entry }>
@@ -21,40 +16,29 @@ export default function CommunityCard(props: {
     <Link
       key={community.entry.community}
       href={`/${community.authorship.author}`}
-      className="block overflow-hidden rounded border border-gray-200 p-6 transition-colors focus-within:ring-2 focus-within:ring-primary-500 focus-within:ring-offset-2 hover:border-primary-400"
+      className="flex items-center overflow-hidden rounded border border-gray-200 p-6 transition-colors focus-within:ring-2 focus-within:ring-primary-500 focus-within:ring-offset-2 hover:border-primary-400"
     >
-      <div className="flex items-center">
-        <Avatar
-          size={20}
-          name={community.authorship.author}
-          value={community.extension?.avatar}
-          className="shrink-0"
-        />
-        <div className="ml-4 w-0 flex-1">
-          <h3 className="w-full truncate break-words text-lg font-medium text-gray-900">
-            {community.name}
-          </h3>
-          {community.extension?.slogan ? (
-            <p className="w-full text-sm text-gray-500 line-clamp-2">
-              {community.extension.slogan}
-            </p>
-          ) : null}
-        </div>
-      </div>
-      <Grid6 className="mt-4 mr-10 text-sm text-gray-500">
+      <Avatar
+        size={24}
+        name={community.authorship.author}
+        value={community.extension?.avatar}
+        className="shrink-0"
+      />
+      <div className="ml-4 flex h-24 w-0 flex-1 flex-col">
+        <h3 className="w-full truncate break-words text-lg font-medium text-gray-900">
+          {community.name}
+        </h3>
+        {community.extension?.slogan ? (
+          <p className="w-full text-sm text-gray-500 line-clamp-2">
+            {community.extension.slogan}
+          </p>
+        ) : null}
+        <div className="h-0 flex-1" />
         <div className="col-span-2 flex items-center">
-          <BriefcaseIcon className="mr-2 h-4 w-4 shrink-0" />
-          {community.workgroups?.length || 0}
-        </div>
-        <div className="col-span-2 flex items-center">
-          <HandRaisedIcon className="mr-2 h-4 w-4 shrink-0" />
-          {community.entry.proposals}
-        </div>
-        <div className="col-span-2 flex items-center">
-          <BookmarkIcon className="mr-2 h-4 w-4 shrink-0" />
+          <BookmarkIcon className="mr-1 h-4 w-4 shrink-0 text-gray-400" />
           {community.entry.subscribers}
         </div>
-      </Grid6>
+      </div>
     </Link>
   )
 }
