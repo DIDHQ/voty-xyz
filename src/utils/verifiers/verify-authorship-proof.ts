04f03e6db73a01c4c5f2c3f482356d777b8d7aa6
Aliez <renzholy@gmail.com>
2023-02-27 11:44:43 +0800

    update

--- src/utils/verifiers/verify-authorship-proof.ts
+++ src/utils/verifiers/verify-authorship-proof.ts
@@ -1,15 +1,15 @@
 import { TRPCError } from '@trpc/server'
 
 import { checkDidAuthorshipProof } from '../did'
-import { Authorized, Authorship } from '../schemas/authorship'
-import { Proof, Proved } from '../schemas/proof'
+import { Authorized } from '../schemas/authorship'
+import { Proved } from '../schemas/proof'
 import { verifyDocument } from '../signature'
 import { isTestnet } from '../constants'
 import { verifyMessage } from '../sdks/ethers'
 
 export default async function verifyAuthorshipProof<T extends object>(
   document: Proved<Authorized<T>>,
-): Promise<{ authorship: Authorship; proof: Proof }> {
+): Promise<void> {
   const { proof, ...rest } = document
 
   if ((rest.authorship.testnet || false) !== isTestnet) {
@@ -26,6 +26,4 @@ export default async function verifyAuthorshipProof<T extends object>(
   if (!(await checkDidAuthorshipProof(rest.authorship, proof))) {
     throw new TRPCError({ code: 'FORBIDDEN', message: 'invalid authorship' })
   }
-
-  return { authorship: rest.authorship, proof }
 }
