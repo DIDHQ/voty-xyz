7e2168c2c51bde3bf3096301735dec9dcecee2b9
Aliez <renzholy@gmail.com>
2023-02-27 10:40:33 +0800

    Update workgroup.tsx

--- src/components/layouts/workgroup.tsx
+++ src/components/layouts/workgroup.tsx
@@ -5,7 +5,7 @@ import WorkgroupNav from '../workgroup-nav'
 export default function WorkgroupLayout(props: { children: ReactNode }) {
   return (
     <>
-      <WorkgroupNav className="sticky top-0 z-30 pt-6 sm:top-18" />
+      <WorkgroupNav className="sticky top-0 z-30 -mx-6 px-6 pt-6 sm:top-18" />
       {props.children}
     </>
   )
