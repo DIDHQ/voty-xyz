2125276e7e42659a249e07b255cb5ae1153431d5
Aliez <renzholy@gmail.com>
2023-02-28 18:48:14 +0800

    github & twitter

--- src/components/basic/dropdown.tsx
+++ src/components/basic/dropdown.tsx
@@ -17,7 +17,7 @@ export default function Dropdown(props: {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-36 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
           {props.children}
         </Menu.Items>
       </Transition>
--- src/components/info-button.tsx
+++ src/components/info-button.tsx
@@ -1,10 +1,11 @@
 import { Menu } from '@headlessui/react'
 import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
-import { BugAntIcon, InformationCircleIcon } from '@heroicons/react/24/outline'
+import { InformationCircleIcon } from '@heroicons/react/24/outline'
 import clsx from 'clsx'
 import Link from 'next/link'
 
 import Dropdown from './basic/dropdown'
+import { GitHubIcon, TwitterIcon } from './icons'
 
 export default function InfoButton(props: { className?: string }) {
   return (
@@ -38,20 +39,39 @@ export default function InfoButton(props: { className?: string }) {
             </Link>
           )}
         </Menu.Item>
+      </div>
+      <div className="py-1">
         <Menu.Item>
           {({ active }) => (
             <a
-              href="https://github.com/VotyXYZ/voty-xyz/issues/new"
+              href="https://github.com/VotyXYZ/voty-xyz"
               className={clsx(
                 active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                 'group flex items-center px-4 py-2 text-sm',
               )}
             >
-              <BugAntIcon
+              <GitHubIcon
                 className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
                 aria-hidden="true"
               />
-              Bug Report
+              GitHub
+            </a>
+          )}
+        </Menu.Item>
+        <Menu.Item>
+          {({ active }) => (
+            <a
+              href="https://twitter.com/voty_xyz"
+              className={clsx(
+                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                'group flex items-center px-4 py-2 text-sm',
+              )}
+            >
+              <TwitterIcon
+                className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                aria-hidden="true"
+              />
+              Twitter
             </a>
           )}
         </Menu.Item>
