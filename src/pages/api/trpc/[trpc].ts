e5a24432be113db3cd785bce6f57f7caac82cd4b
Aliez <renzholy@gmail.com>
2023-02-28 16:37:57 +0800

    Update [trpc].ts

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,6 +2,18 @@ import { createNextApiHandler } from '@trpc/server/adapters/next'
 
 import { appRouter } from '../../../server/routers/_app'
 
+const ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
 export default createNextApiHandler({
   router: appRouter,
+  responseMeta({ type, errors }) {
+    if (errors.length === 0 && type === 'query') {
+      return {
+        headers: {
+          'Cache-Control': `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+        },
+      }
+    }
+    return {}
+  },
 })
