caf11023004a93d0261a2e3d719f2ac6572eee78
Aliez <renzholy@gmail.com>
2023-02-28 12:07:33 +0800

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,10 +15,7 @@ export default function MyDocument() {
 
         <meta name="application-name" content={documentTitle} />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content={documentTitle} />
         <meta name="description" content={description} />
         <meta name="format-detection" content="telephone=no" />
