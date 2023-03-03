3cdc135daa010b321ab2497b6f2ff40af7f3d460
Aliez <renzholy@gmail.com>
2023-03-03 14:08:38 +0800

    Update [trpc].ts

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,4 +1,5 @@
 import { createNextApiHandler } from '@trpc/server/adapters/next'
+import * as Sentry from '@sentry/nextjs'
 
 import { appRouter } from '../../../server/routers/_app'
 import { cacheControl } from '../../../utils/constants'
@@ -12,6 +13,11 @@ export default createNextApiHandler({
     return {}
   },
   onError({ type, path, input, error }) {
+    Sentry.withScope((scope) => {
+      scope.setFingerprint([type, path || ''])
+      scope.setExtra('input', input)
+      Sentry.captureException(error)
+    })
     console.error('TRPC Error:', type, path, input, error)
   },
 })
