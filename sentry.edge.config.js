e53d008d1aaf20e72981d54a5441678ddea1ecad
Aliez <renzholy@gmail.com>
2023-03-06 21:53:59 +0800

    disable sentry in development

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -1,6 +1,9 @@
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn:
+    process.env.NODE_ENV === 'development'
+      ? undefined
+      : process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: 1.0,
 })
