6ffab9ba07ea812a957420477c4144a33ceb56d0
Aliez <renzholy@gmail.com>
2023-03-08 14:15:01 +0800

    remove sidebar

--- src/components/layouts/shell.tsx
+++ src/components/layouts/shell.tsx
@@ -1,7 +1,6 @@
 import dynamic from 'next/dynamic'
 import { ReactNode } from 'react'
 
-import Sidebar from '../sidebar'
 import Toolbar from '../toolbar'
 
 const Banner = dynamic(() => import('../banner'), { ssr: false })
@@ -9,14 +8,9 @@ const Banner = dynamic(() => import('../banner'), { ssr: false })
 export default function ShellLayout(props: { children: ReactNode }) {
   return (
     <>
-      <Sidebar className="fixed left-0 top-0 z-20 hidden h-screen w-18 flex-col items-center border-r border-gray-200 bg-white sm:flex" />
-      <Toolbar
-        className={
-          'sticky top-0 z-20 flex w-full justify-center border-b border-gray-200 bg-white/80 backdrop-blur pt-safe'
-        }
-      />
+      <Toolbar className={'sticky top-0 z-20'} />
       <Banner />
-      <div className="flex w-full justify-center pb-safe sm:pl-18">
+      <div className="flex w-full justify-center pb-safe">
         <div className="flex w-full max-w-5xl flex-col items-start px-6 pb-32 sm:flex-row">
           {props.children}
         </div>
