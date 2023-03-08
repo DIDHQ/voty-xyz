ae519b62c0dc3c294b409775ab308bcdef70f51c
Aliez <renzholy@gmail.com>
2023-03-08 14:08:45 +0800

    Update permalink.ts

--- src/utils/permalink.ts
+++ src/utils/permalink.ts
@@ -6,10 +6,6 @@ export function permalink2Id(permalink: string) {
   return permalink.replace(/^ar:\/\//, '')
 }
 
-export function permalink2Url(permalink: string) {
-  return `https://arweave.net/${permalink2Id(permalink)}`
-}
-
 export function permalink2Explorer(permalink: string) {
   return `https://viewblock.io/arweave/tx/${permalink2Id(permalink)}`
 }
