30d1e611d88cbd293ac496e16e8f84c6f8bb6fdf
Aliez <renzholy@gmail.com>
2023-03-09 10:49:46 +0800

    update create page

--- src/components/community-form.tsx
+++ src/components/community-form.tsx
@@ -70,7 +70,11 @@ export default function CommunityForm(props: {
       </Notification>
       <Form className={clsx('pt-8', props.className)}>
         <FormSection
-          title={isNewCommunity ? 'New community' : 'Basic Information'}
+          title={
+            isNewCommunity
+              ? `New community of ${props.author}`
+              : 'Basic Information'
+          }
         >
           <Grid6>
             <GridItem6>
--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -1,19 +1,21 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo } from 'react'
+import clsx from 'clsx'
+import { useRouter } from 'next/router'
 
 import Button from '../../components/basic/button'
-import DidCombobox from '../../components/did-combobox'
+import { DidOption } from '../../components/did-combobox'
 import LoadingBar from '../../components/basic/loading-bar'
 import useDids from '../../hooks/use-dids'
 import useWallet from '../../hooks/use-wallet'
 import { documentTitle, isTestnet } from '../../utils/constants'
 import { trpc } from '../../utils/trpc'
+import ConnectButton from '../../components/connect-button'
 
 export default function CreateCommunityPage() {
-  const { account, connect } = useWallet()
+  const router = useRouter()
+  const { account } = useWallet()
   const { data, isLoading } = useDids(account)
-  const [entry, setEntry] = useState('')
   const dids = useMemo(
     () => data?.filter((did) => did.indexOf('.') === did.lastIndexOf('.')),
     [data],
@@ -30,9 +32,6 @@ export default function CreateCommunityPage() {
         : undefined,
     [dids, existences],
   )
-  useEffect(() => {
-    setEntry(didOptions?.find(({ disabled }) => !disabled)?.did || '')
-  }, [didOptions])
 
   return (
     <>
@@ -41,58 +40,65 @@ export default function CreateCommunityPage() {
       </Head>
       <LoadingBar loading={isLoading || isExistencesLoading} />
       <div className="w-full bg-white">
-        <div className="py-24 sm:px-6 sm:py-32">
+        <div className="pt-8 sm:px-6 sm:pt-16">
           <div className="mx-auto text-center">
             <h2 className="text-4xl font-bold tracking-tight text-gray-900">
               Create a community
             </h2>
             <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600">
-              {didOptions?.length === 0
-                ? 'You need a DID to create community'
-                : 'to hear valuable voices from your community members'}
+              to hear valuable voices from your community members
             </p>
+
             <div className="mt-10 flex flex-col items-center space-y-6">
-              {didOptions?.length === 0 ? (
-                <a
-                  href={
-                    isTestnet
-                      ? 'https://test2f7a872b.did.id/explorer'
-                      : 'https://app.did.id/explorer'
-                  }
-                >
-                  <Button large primary>
-                    Register →
-                  </Button>
-                </a>
-              ) : (
+              {account ? (
                 <>
-                  <DidCombobox
-                    label="Select a DID as your community entry"
-                    options={didOptions}
-                    value={entry}
-                    onChange={setEntry}
-                    onClick={connect}
-                  />
-                  {entry ? (
-                    <Link href={`/create/${entry}`}>
-                      <Button large primary>
-                        Next →
-                      </Button>
-                    </Link>
-                  ) : (
-                    <a
-                      href={
-                        isTestnet
-                          ? 'https://test2f7a872b.did.id/explorer'
-                          : 'https://app.did.id/explorer'
-                      }
-                    >
-                      <Button large primary>
-                        Register →
-                      </Button>
-                    </a>
-                  )}
+                  {didOptions?.length ? (
+                    <>
+                      <div>
+                        <span className="mb-1 block text-sm font-medium text-gray-700">
+                          Select a DID as your community entry
+                        </span>
+                        <div className="max-h-80 overflow-auto rounded border py-1 text-base focus:outline-none sm:text-sm">
+                          {didOptions?.map((didOption) => (
+                            <div
+                              key={didOption.did}
+                              onClick={() =>
+                                router.push(`/create/${didOption.did}`)
+                              }
+                              className={clsx(
+                                'py-2 px-3 hover:bg-gray-100',
+                                didOption.disabled
+                                  ? 'cursor-not-allowed'
+                                  : 'cursor-pointer',
+                              )}
+                            >
+                              <DidOption
+                                disabled={didOption.disabled}
+                                text={didOption.did}
+                              />
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                      <span className="mb-1 block text-sm font-medium text-gray-400">
+                        or
+                      </span>
+                    </>
+                  ) : null}
+                  <a
+                    href={
+                      isTestnet
+                        ? 'https://test2f7a872b.did.id/explorer'
+                        : 'https://app.did.id/explorer'
+                    }
+                  >
+                    <Button large primary>
+                      Register →
+                    </Button>
+                  </a>
                 </>
+              ) : (
+                <ConnectButton />
               )}
             </div>
           </div>
