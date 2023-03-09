24cc8a1c609454bbe61f1957a1187cb75a6195ee
Aliez <renzholy@gmail.com>
2023-03-09 22:37:21 +0800

    Update settings.tsx

--- src/pages/[entry]/settings.tsx
+++ src/pages/[entry]/settings.tsx
@@ -3,9 +3,9 @@ import { useRouter } from 'next/router'
 
 import CommunityForm from '../../components/community-form'
 import useRouterQuery from '../../hooks/use-router-query'
-import CommunityLayout from '../../components/layouts/community'
 import { trpc } from '../../utils/trpc'
 import LoadingBar from '../../components/basic/loading-bar'
+import TextButton from '../../components/basic/text-button'
 
 export default function CommunitySettingsPage() {
   const router = useRouter()
@@ -24,14 +24,21 @@ export default function CommunitySettingsPage() {
   }, [query.entry, refetch, router])
 
   return (
-    <CommunityLayout>
+    <>
       <LoadingBar loading={isLoading} />
-      <CommunityForm
-        author={query.entry}
-        initialValue={community || undefined}
-        onSuccess={handleSuccess}
-        className="flex w-full flex-col"
-      />
-    </CommunityLayout>
+      <div className="w-full">
+        <TextButton href={`/${query.entry}/about`} className="mt-6 sm:mt-8">
+          <h2 className="text-[1rem] font-semibold leading-6">← Back</h2>
+        </TextButton>
+        {community ? (
+          <CommunityForm
+            author={query.entry}
+            initialValue={community}
+            onSuccess={handleSuccess}
+            className="flex w-full flex-col"
+          />
+        ) : null}
+      </div>
+    </>
   )
 }
