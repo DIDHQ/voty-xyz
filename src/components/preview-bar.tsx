12f1a638926ea75dc46d52389a87e74d72cc0a46
Aliez <renzholy@gmail.com>
2023-03-21 17:04:03 +0800

    add atoms

--- src/components/preview-bar.tsx
+++ src/components/preview-bar.tsx
@@ -4,8 +4,12 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
 import useSignDocument from '../hooks/use-sign-document'
-import { previewCommunityAtom } from '../utils/atoms'
-import { Community } from '../utils/schemas/community'
+import {
+  previewCommunityAtom,
+  previewOptionAtom,
+  previewProposalAtom,
+} from '../utils/atoms'
+import { isCommunity, isOption, isProposal } from '../utils/data-type'
 import { trpc } from '../utils/trpc'
 import { Preview } from '../utils/types'
 import Button from './basic/button'
@@ -15,56 +19,77 @@ import TextButton from './basic/text-button'
 export default function PreviewBar() {
   const router = useRouter()
   const [previewCommunity, setPreviewCommunity] = useAtom(previewCommunityAtom)
+  const [previewProposal, setPreviewProposal] = useAtom(previewProposalAtom)
+  const [previewOption, setPreviewOption] = useAtom(previewOptionAtom)
+  const document = previewCommunity || previewProposal || previewOption
+  const preview = document?.preview
   useEffect(() => {
     function handler(url: string) {
-      if (
-        previewCommunity &&
-        url !== previewCommunity.preview.from &&
-        url !== previewCommunity.preview.to
-      ) {
+      if (preview && url !== preview.from && url !== preview.to) {
         setPreviewCommunity(undefined)
+        setPreviewProposal(undefined)
+        setPreviewOption(undefined)
       }
     }
     router.events.on('routeChangeComplete', handler)
     return () => {
       router.events.off('routeChangeComplete', handler)
     }
-  }, [previewCommunity, router.events, setPreviewCommunity])
-  const signDocument = useSignDocument(
-    previewCommunity?.preview.author,
-    previewCommunity?.preview.template,
+  }, [
+    router.events,
+    preview,
+    setPreviewCommunity,
+    setPreviewProposal,
+    setPreviewOption,
+  ])
+  const signDocument = useSignDocument(preview?.author, preview?.template)
+  const { mutateAsync: mutateCommunity } = trpc.community.create.useMutation()
+  const { mutateAsync: mutateProposal } = trpc.proposal.create.useMutation()
+  const { mutateAsync: mutateOption } = trpc.option.create.useMutation()
+  const handleSubmit = useMutation<void, Error, object & { preview: Preview }>(
+    async ({ preview, ...document }) => {
+      if (isCommunity(document)) {
+        const signed = await signDocument(document)
+        if (signed) {
+          await mutateCommunity(signed)
+          setPreviewCommunity(undefined)
+          router.reload()
+        }
+      }
+      if (isProposal(document)) {
+        const signed = await signDocument(document)
+        if (signed) {
+          await mutateProposal(signed)
+          setPreviewProposal(undefined)
+          router.reload()
+        }
+      }
+      if (isOption(document)) {
+        const signed = await signDocument(document)
+        if (signed) {
+          await mutateOption(signed)
+          setPreviewOption(undefined)
+          router.reload()
+        }
+      }
+    },
   )
-  const { mutateAsync } = trpc.community.create.useMutation()
-  const handleSubmit = useMutation<
-    void,
-    Error,
-    Community & { preview: Preview }
-  >(async ({ preview, ...community }) => {
-    const signed = await signDocument(community)
-    if (signed) {
-      await mutateAsync(signed)
-      setPreviewCommunity(undefined)
-      router.reload()
-    }
-  })
 
   return (
     <>
       <Notification show={handleSubmit.isError}>
         {handleSubmit.error?.message}
       </Notification>
-      {previewCommunity?.preview.to === router.asPath ? (
+      {preview?.to === router.asPath ? (
         <footer className="fixed inset-x-0 bottom-0 h-18 border-t bg-primary-500">
           <div className="mx-auto flex h-18 max-w-5xl items-center justify-between px-6">
-            <TextButton white href={previewCommunity?.preview.from}>
+            <TextButton white href={preview.from}>
               ← Back
             </TextButton>
             <Button
-              disabled={!previewCommunity}
+              disabled={!preview}
               loading={handleSubmit.isLoading}
-              onClick={() =>
-                previewCommunity ? handleSubmit.mutate(previewCommunity) : null
-              }
+              onClick={() => (document ? handleSubmit.mutate(document) : null)}
             >
               Submit
             </Button>
