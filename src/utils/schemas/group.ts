59f00c2cb8b918fc60588c34522f8d315389d167
Aliez <renzholy@gmail.com>
2023-04-04 19:25:30 +0800

    change base

--- src/utils/schemas/group.ts
+++ src/utils/schemas/group.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 import { booleanSetsSchema, decimalSetsSchema } from './sets'
 
-export const workgroupSchema = z.object({
+export const groupSchema = z.object({
   id: z.string().min(1, 'Required'),
   name: z.string().min(1, 'Required'),
   permission: z.object({
@@ -14,35 +14,9 @@ export const workgroupSchema = z.object({
     voting: z.number().int().min(300, 'Minium 5 minutes'),
   }),
   extension: z.object({
-    type: z.literal('workgroup'),
     introduction: z.string().max(160, 'Maximum 160 characters').optional(),
     criteria_for_approval: z.string().min(1, 'Required'),
   }),
 })
-
-export const grantSchema = z.object({
-  id: z.string().min(1, 'Required'),
-  name: z.string().min(1, 'Required'),
-  permission: z.object({
-    proposing: booleanSetsSchema,
-    adding_option: booleanSetsSchema,
-    voting: decimalSetsSchema,
-  }),
-  duration: z.object({
-    announcing: z.number().int().min(300, 'Minium 5 minutes'),
-    adding_option: z.number().int().min(300, 'Minium 5 minutes'),
-    voting: z.number().int().min(300, 'Minium 5 minutes'),
-  }),
-  extension: z.object({
-    type: z.literal('grant'),
-    introduction: z.string().max(160, 'Maximum 160 characters').optional(),
-  }),
-})
-
-export const groupSchema = z.union([workgroupSchema, grantSchema])
-
-export type Workgroup = z.infer<typeof workgroupSchema>
-
-export type Grant = z.infer<typeof grantSchema>
 
 export type Group = z.infer<typeof groupSchema>
