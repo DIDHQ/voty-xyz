6d1199fc9893ac1836b0c5526e251c8631008489
Aliez <renzholy@gmail.com>
2023-04-10 21:28:00 +0800

    Update radio-group.tsx

--- src/components/basic/radio-group.tsx
+++ src/components/basic/radio-group.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx'
 
 export default function RadioGroup(props: {
-  options: { value: string; name: string; description?: string }[]
+  options: { value: string; name: string }[]
   disabled?: boolean
   value: string
   onChange(value: string): void
@@ -12,27 +12,29 @@ export default function RadioGroup(props: {
   return (
     <div className={clsx('space-y-2', props.className)}>
       {props.options.map((option) => (
-        <div key={option.value} className="relative flex items-start">
+        <div
+          key={option.value}
+          onClick={() => (props.disabled ? null : onChange(option.value))}
+          className="relative flex cursor-pointer items-start"
+        >
           <div className="flex h-5 items-center">
             <input
               type="radio"
               disabled={props.disabled}
               checked={props.value === option.value}
               onChange={() => onChange(option.value)}
-              className="h-4 w-4 border-gray-300 text-primary-600 focus:ring-primary-300 disabled:cursor-not-allowed disabled:bg-gray-50 checked:disabled:bg-primary-600"
+              className="h-4 w-4 cursor-pointer border-gray-300 text-primary-600 focus:ring-primary-300 disabled:cursor-not-allowed disabled:bg-gray-50 checked:disabled:bg-primary-600"
             />
           </div>
           <div className="ml-3 text-sm">
             <label
-              onClick={() => (props.disabled ? null : onChange(option.value))}
               className={clsx(
-                'font-medium text-gray-700',
-                props.disabled ? 'cursor-not-allowed' : undefined,
+                'text-gray-700',
+                props.disabled ? 'cursor-not-allowed' : 'cursor-pointer',
               )}
             >
               {option.name}
             </label>
-            <p className="text-gray-500">{option.description}</p>
           </div>
         </div>
       ))}
