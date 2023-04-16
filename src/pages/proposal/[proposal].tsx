0f45f3e4c91d97e7382076f92775802ae2253b0e
Aliez <renzholy@gmail.com>
2023-04-16 10:12:24 +0800

    Update [proposal].tsx

--- src/pages/proposal/[proposal].tsx
+++ src/pages/proposal/[proposal].tsx
@@ -4,7 +4,6 @@ import { compact } from 'lodash-es'
 import { useInView } from 'react-intersection-observer'
 import Head from 'next/head'
 import { useAtomValue } from 'jotai'
-import { useRouter } from 'next/router'
 
 import useGroup from '../../hooks/use-group'
 import { stringifyChoice } from '../../utils/choice'
@@ -23,7 +22,6 @@ import { Proposal } from '../../utils/schemas/proposal'
 
 export default function ProposalPage() {
   const query = useRouterQuery<['proposal']>()
-  const router = useRouter()
   const previewProposal = useAtomValue(previewProposalAtom)
   const { data, isLoading, refetch } = trpc.proposal.getByPermalink.useQuery(
     { permalink: query.proposal },
@@ -77,8 +75,7 @@ export default function ProposalPage() {
   const handleSuccess = useCallback(async () => {
     await refetch()
     await refetchList()
-    router.reload()
-  }, [refetch, refetchList, router])
+  }, [refetch, refetchList])
 
   return (
     <>
