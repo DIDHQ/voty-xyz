6e10fdc044fafdc29d8ffeb4d0769eaeed0b9e27
Aliez <renzholy@gmail.com>
2023-04-25 20:22:10 +0900

    update base

--- src/utils/schemas/vote.ts
+++ src/utils/schemas/vote.ts
@@ -1,21 +1,16 @@
 import Decimal from 'decimal.js'
 import { z } from 'zod'
 
-import { choiceIsEmpty } from '../choice'
 import { decimalSchema } from './decimal'
 
 export const voteSchema = z.object({
-  proposal: z.string().min(1),
-  choice: z
-    .string()
-    .refine(
-      (choice) =>
-        !choiceIsEmpty('single', choice) && !choiceIsEmpty('approval', choice),
-      { message: 'Empty choice' },
-    ),
-  power: decimalSchema.refine((power) => new Decimal(power).gt(0), {
-    message: 'Negative power not allowed',
-  }),
+  target: z.string().min(1),
+  powers: z.record(
+    z.string(),
+    decimalSchema.refine((power) => new Decimal(power).gt(0), {
+      message: 'Negative power not allowed',
+    }),
+  ),
 })
 
 export type Vote = z.infer<typeof voteSchema>
