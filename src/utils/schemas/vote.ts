ff4579df9888b9c1d33f5fa10a2790e4a1a9513a
Aliez <renzholy@gmail.com>
2023-04-26 15:05:09 +0900

    Revert "update base"
    This reverts commit 6e10fdc044fafdc29d8ffeb4d0769eaeed0b9e27.

--- src/utils/schemas/vote.ts
+++ src/utils/schemas/vote.ts
@@ -1,16 +1,21 @@
 import Decimal from 'decimal.js'
 import { z } from 'zod'
 
+import { choiceIsEmpty } from '../choice'
 import { decimalSchema } from './decimal'
 
 export const voteSchema = z.object({
-  target: z.string().min(1),
-  powers: z.record(
-    z.string(),
-    decimalSchema.refine((power) => new Decimal(power).gt(0), {
-      message: 'Negative power not allowed',
-    }),
-  ),
+  proposal: z.string().min(1),
+  choice: z
+    .string()
+    .refine(
+      (choice) =>
+        !choiceIsEmpty('single', choice) && !choiceIsEmpty('approval', choice),
+      { message: 'Empty choice' },
+    ),
+  power: decimalSchema.refine((power) => new Decimal(power).gt(0), {
+    message: 'Negative power not allowed',
+  }),
 })
 
 export type Vote = z.infer<typeof voteSchema>
