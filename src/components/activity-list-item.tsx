f7151e2e96c7526eaf7572a256b6c66701548b49
Aliez <renzholy@gmail.com>
2023-05-11 20:39:33 +0900

    Update activity-list-item.tsx

--- src/components/activity-list-item.tsx
+++ src/components/activity-list-item.tsx
@@ -4,9 +4,9 @@ import {
   TrophyIcon,
   BoltIcon,
   HandRaisedIcon,
+  QuestionMarkCircleIcon,
 } from '@heroicons/react/20/solid'
-import { useCallback, useMemo } from 'react'
-import { last } from 'lodash-es'
+import { ExoticComponent, ReactNode, useMemo } from 'react'
 import clsx from 'clsx'
 
 import { Activity } from '../utils/schemas/activity'
@@ -14,225 +14,272 @@ import { formatDurationMs } from '../utils/time'
 import TextButton from './basic/text-button'
 import useNow from '../hooks/use-now'
 
-import { permalink2Id } from '../utils/permalink'
+import { permalink2Explorer, permalink2Id } from '../utils/permalink'
 
 export default function ActivityListItem(props: {
   activity: { data: Activity; ts: Date; actor: string }
 }) {
   const { activity } = props
   const now = useNow()
-  const renderAction = useCallback(() => {
+  const action = useMemo<{
+    color: string
+    icon: ExoticComponent<{ className?: string }>
+    children: ReactNode
+    permalink?: string
+  }>(() => {
     switch (activity.data.type) {
       case 'create_community': {
-        return 'imported community'
+        return {
+          color: 'bg-green-100 text-green-500',
+          icon: UserGroupIcon,
+          children: 'imported community',
+          permalink: activity.data.community_permalink,
+        }
       }
       case 'update_community': {
-        return 'updated community'
+        return {
+          color: 'bg-blue-100 text-blue-500',
+          icon: UserGroupIcon,
+          children: 'updated community',
+          permalink: activity.data.community_permalink,
+        }
       }
       case 'create_grant': {
-        return (
-          <>
-            created grant{' '}
-            <TextButton
-              href={`/${activity.data.community_id}/grant/${permalink2Id(
-                activity.data.grant_permalink,
-              )}`}
-              underline
-              className="font-medium text-gray-900"
-            >
-              {activity.data.grant_name}
-            </TextButton>
-          </>
-        )
+        return {
+          color: 'bg-green-100 text-green-500',
+          icon: TrophyIcon,
+          children: (
+            <>
+              created grant{' '}
+              <TextButton
+                href={`/${activity.data.community_id}/grant/${permalink2Id(
+                  activity.data.grant_permalink,
+                )}`}
+                underline
+                className="font-medium text-gray-900"
+              >
+                {activity.data.grant_name}
+              </TextButton>
+            </>
+          ),
+          permalink: activity.data.grant_permalink,
+        }
       }
       case 'create_grant_proposal': {
-        return (
-          <>
-            made a proposal{' '}
-            <TextButton
-              href={`/${activity.data.community_id}/grant/${permalink2Id(
-                activity.data.grant_permalink,
-              )}/proposal/${permalink2Id(
-                activity.data.grant_proposal_permalink,
-              )}`}
-              underline
-              className="font-medium text-gray-900"
-            >
-              {activity.data.grant_name}
-            </TextButton>{' '}
-            in grant{' '}
-            <TextButton
-              href={`/${activity.data.community_id}/grant/${permalink2Id(
-                activity.data.grant_permalink,
-              )}`}
-              underline
-              className="font-medium text-gray-900"
-            >
-              {activity.data.grant_proposal_title}
-            </TextButton>
-          </>
-        )
+        return {
+          color: 'bg-green-100 text-green-500',
+          icon: HandRaisedIcon,
+          children: (
+            <>
+              made a proposal{' '}
+              <TextButton
+                href={`/${activity.data.community_id}/grant/${permalink2Id(
+                  activity.data.grant_permalink,
+                )}/proposal/${permalink2Id(
+                  activity.data.grant_proposal_permalink,
+                )}`}
+                underline
+                className="font-medium text-gray-900"
+              >
+                {activity.data.grant_name}
+              </TextButton>{' '}
+              in grant{' '}
+              <TextButton
+                href={`/${activity.data.community_id}/grant/${permalink2Id(
+                  activity.data.grant_permalink,
+                )}`}
+                underline
+                className="font-medium text-gray-900"
+              >
+                {activity.data.grant_proposal_title}
+              </TextButton>
+            </>
+          ),
+          permalink: activity.data.grant_proposal_permalink,
+        }
       }
       case 'create_grant_proposal_vote': {
-        return (
-          <>
-            voted for proposal{' '}
-            <TextButton
-              href={`/${activity.data.community_id}/grant/${permalink2Id(
-                activity.data.grant_permalink,
-              )}/proposal/${permalink2Id(
-                activity.data.grant_proposal_permalink,
-              )}`}
-              underline
-              className="font-medium text-gray-900"
-            >
-              {activity.data.grant_name}
-            </TextButton>{' '}
-            in grant{' '}
-            <TextButton
-              href={`/${activity.data.community_id}/grant/${permalink2Id(
-                activity.data.grant_permalink,
-              )}`}
-              underline
-              className="font-medium text-gray-900"
-            >
-              {activity.data.grant_proposal_title}
-            </TextButton>
-          </>
-        )
+        return {
+          color: 'bg-yellow-100 text-yellow-500',
+          icon: BoltIcon,
+          children: (
+            <>
+              voted for proposal{' '}
+              <TextButton
+                href={`/${activity.data.community_id}/grant/${permalink2Id(
+                  activity.data.grant_permalink,
+                )}/proposal/${permalink2Id(
+                  activity.data.grant_proposal_permalink,
+                )}`}
+                underline
+                className="font-medium text-gray-900"
+              >
+                {activity.data.grant_name}
+              </TextButton>{' '}
+              in grant{' '}
+              <TextButton
+                href={`/${activity.data.community_id}/grant/${permalink2Id(
+                  activity.data.grant_permalink,
+                )}`}
+                underline
+                className="font-medium text-gray-900"
+              >
+                {activity.data.grant_proposal_title}
+              </TextButton>
+            </>
+          ),
+          permalink: activity.data.grant_proposal_vote_permalink,
+        }
       }
       case 'create_group': {
-        return (
-          <>
-            created group{' '}
-            <TextButton
-              href={`/${activity.data.community_id}/group/${activity.data.group_id}`}
-              underline
-              className="font-medium text-gray-900"
-            >
-              {activity.data.group_name}
-            </TextButton>
-          </>
-        )
+        return {
+          color: 'bg-green-100 text-green-500',
+          icon: BriefcaseIcon,
+          children: (
+            <>
+              created group{' '}
+              <TextButton
+                href={`/${activity.data.community_id}/group/${activity.data.group_id}`}
+                underline
+                className="font-medium text-gray-900"
+              >
+                {activity.data.group_name}
+              </TextButton>
+            </>
+          ),
+          permalink: activity.data.group_permalink,
+        }
       }
       case 'update_group': {
-        return (
-          <>
-            updated group{' '}
-            <TextButton
-              href={`/${activity.data.community_id}/group/${activity.data.group_id}`}
-              underline
-              className="font-medium text-gray-900"
-            >
-              {activity.data.group_name}
-            </TextButton>
-          </>
-        )
+        return {
+          color: 'bg-blue-100 text-blue-500',
+          icon: BriefcaseIcon,
+          children: (
+            <>
+              updated group{' '}
+              <TextButton
+                href={`/${activity.data.community_id}/group/${activity.data.group_id}`}
+                underline
+                className="font-medium text-gray-900"
+              >
+                {activity.data.group_name}
+              </TextButton>
+            </>
+          ),
+          permalink: activity.data.group_permalink,
+        }
       }
       case 'delete_group': {
-        return (
-          <>
-            archived group{' '}
-            <span className="font-medium text-gray-900">
-              {activity.data.group_name}
-            </span>
-          </>
-        )
+        return {
+          color: 'bg-red-100 text-red-500',
+          icon: BriefcaseIcon,
+          children: (
+            <>
+              archived group{' '}
+              <span className="font-medium text-gray-900">
+                {activity.data.group_name}
+              </span>
+            </>
+          ),
+        }
       }
       case 'create_group_proposal': {
-        return (
-          <>
-            made a proposal{' '}
-            <TextButton
-              href={`/${activity.data.community_id}/group/${
-                activity.data.group_id
-              }/proposal/${permalink2Id(
-                activity.data.group_proposal_permalink,
-              )}`}
-              underline
-              className="font-medium text-gray-900"
-            >
-              {activity.data.group_name}
-            </TextButton>{' '}
-            in group{' '}
-            <TextButton
-              href={`/${activity.data.community_id}/group/${activity.data.group_id}`}
-              underline
-              className="font-medium text-gray-900"
-            >
-              {activity.data.group_proposal_title}
-            </TextButton>
-          </>
-        )
+        return {
+          color: 'bg-green-100 text-green-500',
+          icon: HandRaisedIcon,
+          children: (
+            <>
+              made a proposal{' '}
+              <TextButton
+                href={`/${activity.data.community_id}/group/${
+                  activity.data.group_id
+                }/proposal/${permalink2Id(
+                  activity.data.group_proposal_permalink,
+                )}`}
+                underline
+                className="font-medium text-gray-900"
+              >
+                {activity.data.group_name}
+              </TextButton>{' '}
+              in group{' '}
+              <TextButton
+                href={`/${activity.data.community_id}/group/${activity.data.group_id}`}
+                underline
+                className="font-medium text-gray-900"
+              >
+                {activity.data.group_proposal_title}
+              </TextButton>
+            </>
+          ),
+          permalink: activity.data.group_proposal_permalink,
+        }
       }
       case 'create_group_proposal_vote': {
-        return (
-          <>
-            voted in proposal{' '}
-            <TextButton
-              href={`/${activity.data.community_id}/group/${
-                activity.data.group_id
-              }/proposal/${permalink2Id(
-                activity.data.group_proposal_permalink,
-              )}`}
-              underline
-              className="font-medium text-gray-900"
-            >
-              {activity.data.group_name}
-            </TextButton>{' '}
-            in group{' '}
-            <TextButton
-              href={`/${activity.data.community_id}/group/${activity.data.group_id}`}
-              underline
-              className="font-medium text-gray-900"
-            >
-              {activity.data.group_proposal_title}
-            </TextButton>
-          </>
-        )
+        return {
+          color: 'bg-yellow-100 text-yellow-500',
+          icon: BoltIcon,
+          children: (
+            <>
+              voted in proposal{' '}
+              <TextButton
+                href={`/${activity.data.community_id}/group/${
+                  activity.data.group_id
+                }/proposal/${permalink2Id(
+                  activity.data.group_proposal_permalink,
+                )}`}
+                underline
+                className="font-medium text-gray-900"
+              >
+                {activity.data.group_name}
+              </TextButton>{' '}
+              in group{' '}
+              <TextButton
+                href={`/${activity.data.community_id}/group/${activity.data.group_id}`}
+                underline
+                className="font-medium text-gray-900"
+              >
+                {activity.data.group_proposal_title}
+              </TextButton>
+            </>
+          ),
+          permalink: activity.data.group_proposal_vote_permalink,
+        }
       }
       default: {
-        return 'did something'
+        return {
+          color: 'bg-gray-100 text-gray-500',
+          icon: QuestionMarkCircleIcon,
+          children: 'did something',
+        }
       }
     }
   }, [activity.data])
-  const icon = useMemo(
-    () =>
-      ({
-        community: <UserGroupIcon className="h-5 w-5" />,
-        group: <BriefcaseIcon className="h-5 w-5" />,
-        grant: <TrophyIcon className="h-5 w-5" />,
-        proposal: <HandRaisedIcon className="h-5 w-5" />,
-        vote: <BoltIcon className="h-5 w-5" />,
-      }[last(activity.data.type.split('_'))!]),
-    [activity.data.type],
-  )
-  const color = useMemo(
-    () =>
-      ({
-        create: 'bg-green-100 text-green-500',
-        update: 'bg-blue-100 text-blue-500',
-        delete: 'bg-red-100 text-red-500',
-      }[activity.data.type.split('_')[0]]),
-    [activity.data.type],
-  )
 
   return (
     <div className="flex items-center space-x-2">
       <div
         className={clsx(
           'flex h-8 w-8 items-center justify-center rounded-full ring-8 ring-white',
-          color,
+          action.color,
         )}
       >
-        {icon}
+        {<action.icon className="h-5 w-5" />}
       </div>
       <div className="min-w-0 flex-1 py-1.5 text-sm text-gray-500">
         <span className="font-medium text-gray-900">{activity.actor}</span>{' '}
-        {renderAction()}{' '}
-        <span className="whitespace-nowrap">
-          {formatDurationMs(now.getTime() - activity.ts.getTime())} ago
-        </span>
+        {action.children}{' '}
+        {action.permalink ? (
+          <a
+            href={permalink2Explorer(action.permalink)}
+            className="whitespace-nowrap"
+          >
+            {formatDurationMs(now.getTime() - activity.ts.getTime())} ago
+          </a>
+        ) : (
+          <span className="whitespace-nowrap">
+            {formatDurationMs(now.getTime() - activity.ts.getTime())} ago
+          </span>
+        )}
       </div>
     </div>
   )
