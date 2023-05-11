8ec9934e82529e47557d4c1902d24303d5c5579f
Aliez <renzholy@gmail.com>
2023-05-11 16:36:51 +0900

    Update activity.ts

--- src/utils/schemas/activity.ts
+++ src/utils/schemas/activity.ts
@@ -2,31 +2,20 @@ import { z } from 'zod'
 
 export const activitySchema = z.discriminatedUnion('type', [
   z.object({
-    type: z.literal('create_community'),
+    type: z.union([
+      z.literal('create_community'),
+      z.literal('modify_community'),
+    ]),
     community_id: z.string(),
     community_permalink: z.string(),
     community_name: z.string(),
   }),
   z.object({
-    type: z.literal('modify_community'),
-    community_id: z.string(),
-    community_permalink: z.string(),
-    community_name: z.string(),
-  }),
-  z.object({
-    type: z.literal('create_group'),
-    group_id: z.string(),
-    group_permalink: z.string(),
-    group_name: z.string(),
-  }),
-  z.object({
-    type: z.literal('modify_group'),
-    group_id: z.string(),
-    group_permalink: z.string(),
-    group_name: z.string(),
-  }),
-  z.object({
-    type: z.literal('archive_group'),
+    type: z.union([
+      z.literal('create_group'),
+      z.literal('modify_group'),
+      z.literal('archive_group'),
+    ]),
     group_id: z.string(),
     group_permalink: z.string(),
     group_name: z.string(),
