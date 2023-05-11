17ff42cd2878c3a4cf45f01e4f21ae75c0d21700
Aliez <renzholy@gmail.com>
2023-05-11 12:04:36 +0900

    Update use-now.tsx

--- src/hooks/use-now.tsx
+++ src/hooks/use-now.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from '@tanstack/react-query'
 
 export default function useNow() {
-  const { data = new Date() } = useQuery(['now'], () => new Date())
+  const { data = new Date() } = useQuery(['now'], () => new Date(), {
+    refetchInterval: 10 * 1000,
+  })
   return data
 }
