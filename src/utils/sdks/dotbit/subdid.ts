d238407c165ca860b7580e662c5f794cfbef7d16
Aliez <renzholy@gmail.com>
2023-05-16 15:36:40 +0900

    Create subdid.ts

---
+++ src/utils/sdks/dotbit/subdid.ts
@@ -0,0 +1,11 @@
+import { fetchJson, postJson } from '../../fetcher'
+
+export async function hasEnabledSubDID(did: string) {
+  const { err_no } = await fetchJson<{
+    err_no: number
+  }>(
+    'https://subaccount-api.did.id/v1/custom/script/info',
+    postJson({ account: did }),
+  )
+  return err_no === 0
+}
