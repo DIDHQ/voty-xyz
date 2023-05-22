a08b1a45c777b343edb04bab623db1dbb23fe2b7
Aliez <renzholy@gmail.com>
2023-05-22 19:41:36 +0900

    sanitize

--- src/components/basic/markdown-viewer.tsx
+++ src/components/basic/markdown-viewer.tsx
@@ -1,12 +1,26 @@
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize'
 import remarkGfm from 'remark-gfm'
 
 export default function MarkdownViewer(props: { children?: string }) {
   return props.children ? (
     <ReactMarkdown
       remarkPlugins={[remarkGfm]}
-      rehypePlugins={[rehypeRaw]}
+      rehypePlugins={[
+        rehypeRaw,
+        [
+          rehypeSanitize,
+          {
+            ...defaultSchema,
+            tagNames: [...(defaultSchema.tagNames || []), 'iframe'],
+            attributes: {
+              ...defaultSchema.attributes,
+              iframe: ['src', 'frameborder', 'allow', 'title'],
+            },
+          },
+        ],
+      ]}
       sourcePos
     >
       {props.children}
