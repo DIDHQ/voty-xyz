9686f3d599c788520361f392ee8ef783fbff9e34
Aliez <renzholy@gmail.com>
2023-05-25 12:57:50 +0900

    Update connect-button.tsx

--- src/components/connect-button.tsx
+++ src/components/connect-button.tsx
@@ -2,13 +2,14 @@ import { ConnectButton as RainbowKitConnectButton } from '@rainbow-me/rainbowkit
 import { Menu } from '@headlessui/react'
 import { clsx } from 'clsx'
 import { event } from 'nextjs-google-analytics'
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 import '@rainbow-me/rainbowkit/styles.css'
 
 import useWallet from '../hooks/use-wallet'
 import { coinTypeLogos, coinTypeNames } from '../utils/constants'
 import useRouterQuery from '../hooks/use-router-query'
 import useIsManager from '../hooks/use-is-manager'
+import useDids from '../hooks/use-dids'
 import Button from './basic/button'
 import Avatar from './basic/avatar'
 import Dropdown from './basic/dropdown'
@@ -17,6 +18,14 @@ export default function ConnectButton() {
   const { account, displayAddress, disconnect } = useWallet()
   const query = useRouterQuery<['community_id']>()
   const isManager = useIsManager(query.community_id)
+  const { data: dids } = useDids(account)
+  const isMember = useMemo(
+    () =>
+      !!dids?.find(
+        (did) => !!query.community_id && did.startsWith(query.community_id),
+      ),
+    [dids, query.community_id],
+  )
   useEffect(() => {
     if (account && query.community_id) {
       event('connect_wallet', {
@@ -24,9 +33,10 @@ export default function ConnectButton() {
         coinType: account.coinType,
         communityId: query.community_id,
         isManager,
+        isMember,
       })
     }
-  }, [account, isManager, query.community_id])
+  }, [account, isManager, isMember, query.community_id])
 
   return (
     <RainbowKitConnectButton.Custom>
