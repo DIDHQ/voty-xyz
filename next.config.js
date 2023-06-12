18a442f3d401fc778434fcb652354d8c39d8644b
Aliez <renzholy@gmail.com>
2023-06-12 21:45:18 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,14 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  modularizeImports: {
+    '@heroicons/react/20/solid': {
+      transform: '@heroicons/react/20/solid/{{member}}',
+    },
+    '@heroicons/react/24/outline': {
+      transform: '@heroicons/react/24/outline/{{member}}',
+    },
+  },
 }
 
 module.exports = withSentryConfig(
