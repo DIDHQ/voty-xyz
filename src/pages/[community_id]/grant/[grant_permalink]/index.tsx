e017946a1e8575f5e71bebd08460bf0b0d1e6040
Aliez <renzholy@gmail.com>
2023-06-12 18:10:03 +0800

    Update index.tsx

--- src/pages/[community_id]/grant/[grant_permalink]/index.tsx
+++ src/pages/[community_id]/grant/[grant_permalink]/index.tsx
@@ -1,15 +1,12 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo } from 'react'
 import { compact } from 'lodash-es'
 import Head from 'next/head'
 import { useAtomValue } from 'jotai'
-import { useCollapse } from 'react-collapsed'
 import SuperJSON from 'superjson'
 import { createServerSideHelpers } from '@trpc/react-query/server'
 import { GetServerSidePropsContext } from 'next'
 
 import { trpc } from '@/src/utils/trpc'
-import Article from '@/src/components/basic/article'
-import TextLink from '@/src/components/basic/text-link'
 import LoadingBar from '@/src/components/basic/loading-bar'
 import {
   documentDescription,
@@ -20,22 +17,12 @@ import {
 import useRouterQuery from '@/src/hooks/use-router-query'
 import { previewGrantAtom } from '@/src/utils/atoms'
 import { Grant } from '@/src/utils/schemas/v1/grant'
-import GrantProposalCard from '@/src/components/grant-proposal-card'
-import { getGrantPhase } from '@/src/utils/phase'
-import useStatus from '@/src/hooks/use-status'
-import useNow from '@/src/hooks/use-now'
-import Select from '@/src/components/basic/select'
-import TextButton from '@/src/components/basic/text-button'
-import useWallet from '@/src/hooks/use-wallet'
 import {
   id2Permalink,
   isPermalink,
   permalink2Gateway,
 } from '@/src/utils/permalink'
 import { getImages, getSummary } from '@/src/utils/markdown'
-import MarkdownViewer from '@/src/components/basic/markdown-viewer'
-import GrantInfo from '@/src/components/grant-info'
-import GrantProposalCreateButton from '@/src/components/grant-proposal-create-button'
 import { appRouter } from '@/src/server/routers/_app'
 
 export async function getServerSideProps(
@@ -88,11 +75,6 @@ export default function GrantPage() {
       { permalink: grant?.community },
       { enabled: !!grant?.community, refetchOnWindowFocus: false },
     )
-  const { account } = useWallet()
-  const { data: grantProposals } = trpc.grantProposal.list.useQuery(
-    { grantPermalink: query.grant_permalink, viewer: account?.address },
-    { enabled: !!query.grant_permalink },
-  )
   const title = useMemo(
     () => compact([grant?.name, community?.name, documentTitle]).join(' - '),
     [community?.name, grant?.name],
@@ -111,22 +93,6 @@ export default function GrantPage() {
     }
     return isPermalink(image) ? permalink2Gateway(image) : image
   }, [grant?.introduction])
-  const { data: status } = useStatus(grant?.permalink)
-  const now = useNow()
-  const phase = useMemo(
-    () => getGrantPhase(now, status?.timestamp, grant?.duration),
-    [grant?.duration, now, status?.timestamp],
-  )
-  const options = useMemo(() => ['All', 'Selected'], [])
-  const [option, setOption] = useState(options[0])
-  useEffect(() => {
-    setOption(options[grant?.selectedProposals ? 1 : 0])
-  }, [grant?.selectedProposals, options])
-  const [isExpanded, setExpanded] = useState(false)
-  const { getCollapseProps, getToggleProps } = useCollapse({
-    isExpanded,
-    collapsedHeight: 300,
-  })
 
   return (
     <>
@@ -145,103 +111,6 @@ export default function GrantPage() {
         <meta property="og:image" content={image} />
       </Head>
       <LoadingBar loading={isLoading || isCommunityLoading} />
-      <div className="flex w-full flex-1 flex-col items-start sm:flex-row">
-        <div className="w-full flex-1 pt-6 sm:mr-10 sm:w-0 sm:pt-8">
-          <TextLink
-            disabled={!community || !!previewGrant}
-            href={`/${community?.id}/grant`}
-            className="inline-block"
-          >
-            <h2 className="text-base font-semibold">← Back</h2>
-          </TextLink>
-          {grant && grant?.introduction.split('\n').length >= 4 ? (
-            <>
-              <section {...getCollapseProps()}>
-                <Article className="my-6 sm:my-8">
-                  <h1>{grant?.name || '...'}</h1>
-                  <MarkdownViewer preview={!!previewGrant}>
-                    {grant?.introduction}
-                  </MarkdownViewer>
-                </Article>
-              </section>
-              <TextButton
-                secondary
-                {...getToggleProps({
-                  onClick: () => setExpanded((prevExpanded) => !prevExpanded),
-                })}
-              >
-                {isExpanded ? '↑ Collapse' : '↓ Expand'}
-              </TextButton>
-            </>
-          ) : (
-            <Article className="my-6 sm:my-8">
-              <h1>{grant?.name || '...'}</h1>
-              <MarkdownViewer preview={!!previewGrant}>
-                {grant?.introduction}
-              </MarkdownViewer>
-            </Article>
-          )}
-          <GrantInfo
-            community={community || undefined}
-            grant={grant}
-            className="mb-6 block sm:hidden"
-          />
-          {previewGrant ? null : (
-            <div className="my-6 flex items-center justify-between border-t border-gray-200 pt-6">
-              {grant?.proposals ? (
-                <h2 className="text-2xl font-bold">
-                  {option === options[0]
-                    ? grant.proposals === 1
-                      ? '1 Proposal'
-                      : `${grant.proposals} Proposals`
-                    : grant.selectedProposals === 1
-                    ? '1 Selected proposal'
-                    : `${grant.selectedProposals} Selected proposals`}
-                </h2>
-              ) : (
-                <h2 />
-              )}
-              <div className="flex items-center">
-                {grant?.permission.selecting && grant?.proposals ? (
-                  <Select
-                    options={options}
-                    value={option}
-                    onChange={setOption}
-                  />
-                ) : null}
-                <GrantProposalCreateButton
-                  communityId={query.community_id}
-                  grant={grant}
-                  className="ml-5"
-                />
-              </div>
-            </div>
-          )}
-          <ul className="mt-5 space-y-5">
-            {grantProposals
-              ?.filter(
-                (grantProposal) =>
-                  option === options[0] || grantProposal.selected,
-              )
-              .map((grantProposal) => (
-                <li key={grantProposal.permalink}>
-                  {query.community_id && grant ? (
-                    <GrantProposalCard
-                      communityId={query.community_id}
-                      grantProposal={grantProposal}
-                      phase={phase}
-                    />
-                  ) : null}
-                </li>
-              ))}
-          </ul>
-        </div>
-        <GrantInfo
-          community={community || undefined}
-          grant={grant}
-          className="hidden sm:block"
-        />
-      </div>
     </>
   )
 }
