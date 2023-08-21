51be88c47a563ef62d341e6d62de79c487be98a5
QiongrongJiang <qiongrongjiang@gmail.com>
2023-08-20 20:04:42 -0400

    feat: add loading prop and loading bar

--- src/components/layouts/community.tsx
+++ src/components/layouts/community.tsx
@@ -2,17 +2,30 @@ import { ReactNode } from 'react'
 
 import CommunityInfo from '../community-info'
 import { Container, Main, Sidebar } from '../basic/container'
+import LoadingBar from '../basic/loading-bar'
 
-export default function CommunityLayout(props: { children: ReactNode }) {
+export default function CommunityLayout(props: { 
+  loading?: boolean
+  children: ReactNode
+}) {
+  const {
+    loading = false,
+    children
+  } = props
+  
   return (
     <Container
       hasSidebar>
+      <LoadingBar 
+        loading={loading} />
+         
       <Sidebar>
-        <CommunityInfo />
+        <CommunityInfo
+          loading={loading} />
       </Sidebar>  
       
       <Main>
-        {props.children}
+        {children}
       </Main>
     </Container>
   )
